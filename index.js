1713bfbe51112edf8529ed02b9801131c9abe0b6
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-10-30 23:16:52 -0300

    Setup mongodb connection with mongoose

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const express = require('express')
-const app = express()
+const createApp = require('./createApp')
 
-const graphqlMiddleware = require('./middlewares/graphql')
-const ioMiddleware = require('./middlewares/io')
+createApp().then(app => {
+  const port = process.env.PORT || 5000
 
-ioMiddleware.set(app)
-graphqlMiddleware.set(app)
-
-const port = process.env.PORT || 5000
-
-app.listen(port, () => {
-  // eslint-disable-next-line
-  console.log(`App listening on https://localhost:${port}`)
+  app.listen(port, () => {
+    // eslint-disable-next-line
+    console.log(`App listening on https://localhost:${port}`)
+  })
 })
