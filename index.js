33d157f88b377b3830a1318705c319541fc0a5bd
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-11-01 14:48:11 -0300

    Move startup to index

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const createApp = require('./createApp')
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
 
-createApp().then(app => {
-  const port = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/cramkle'
 
-  app.listen(port, () => {
-    // eslint-disable-next-line
-    console.log(`App listening on https://localhost:${port}`)
+const graphqlMiddleware = require('./middlewares/graphql')
+const ioMiddleware = require('./middlewares/io')
+
+ioMiddleware.set(app)
+graphqlMiddleware.set(app)
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App listening on https://localhost:${PORT}`)
+    })
   })
-})
+  .catch(e => {
+    console.error('Failed to obtain a connection to MongoDB')
+    console.error(e)
+    process.exit(1)
+  })
