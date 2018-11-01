08d3c9c6084d8dde4d1d1443dd2179b401bc4c4f
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-11-01 18:28:01 -0300

    Add root module for models

---
+++ models/index.js
@@ -0,0 +1,19 @@
+const Card = require('./Card')
+const CardModel = require('./CardModel')
+const Template = require('./Template')
+const Field = require('./Field')
+const FieldValue = require('./FieldValue')
+const User = require('./User')
+const Deck = require('./Deck')
+const Note = require('./Note')
+
+module.exports = {
+  Card,
+  CardModel,
+  Template,
+  Field,
+  FieldValue,
+  User,
+  Deck,
+  Note,
+}
