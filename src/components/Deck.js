86e790d2cbab102794d067aac1d5e66cfd929bd5
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-11-02 01:23:52 -0300

    Hopefully the last typescript file I forgot

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,11 +1,7 @@
-import * as React from 'react'
+import React from 'react'
 import Card, { CardPrimaryContent } from '@material/react-card'
 
-import { DeckEntity } from '../types'
-
-interface DeckProps extends DeckEntity {}
-
-const Deck: React.StatelessComponent<DeckProps> = ({ title, description }) => (
+const Deck = ({ title, description }) => (
   <Card outlined>
     <CardPrimaryContent style={{ padding: '.3rem .5rem' }}>
       <h3 className="mdc-typography--headline6">{title}</h3>
