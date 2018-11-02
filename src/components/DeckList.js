cc8a8f2f4724f5b5151eb4f1180a1f4e3c97b834
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-11-02 01:37:01 -0300

    Hook up the decks query with the list component

--- src/components/DeckList.js
+++ src/components/DeckList.js
@@ -1,40 +1,39 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { graphql } from 'react-apollo'
 
 import Deck from './Deck'
+import decksQuery from '../graphql/decksQuery.gql'
 
-export default class DeckList extends Component {
-  static defaultProps = {
-    loading: true,
+const DeckList = ({ data: { loading, decks } }) => {
+  if (loading) {
+    return <p>loading...</p>
   }
 
-  render() {
-    const { loading, decks } = this.props
-
-    if (loading) {
-      return <p>loading...</p>
-    }
-
-    if (decks.length === 0) {
-      return (
-        <p className="mdc-typography--caption">
-          You haven&apos;t created any decks yet
-        </p>
-      )
-    }
-
+  if (decks.length === 0) {
     return (
-      <div className="mdc-layout-grid">
-        <p className="mdc-typography--caption">Decks</p>
-
-        <div className="mdc-layout-grid__inner">
-          {decks.map(deck => (
-            <div key={deck.deckId} className="mdc-layout-grid__cell">
-              <Deck {...deck} />
-            </div>
-          ))}
-        </div>
-      </div>
+      <p className="mdc-typography--caption">
+        You haven&apos;t created any decks yet
+      </p>
     )
   }
+
+  return (
+    <div className="mdc-layout-grid">
+      <p className="mdc-typography--caption">Decks</p>
+
+      <div className="mdc-layout-grid__inner">
+        {decks.map(deck => (
+          <div key={deck.deckId} className="mdc-layout-grid__cell">
+            <Deck {...deck} />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
+DeckList.defaultProps = {
+  loading: true,
+}
+
+export default graphql(decksQuery)(DeckList)
