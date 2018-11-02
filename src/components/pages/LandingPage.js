5f0f4b448b99fe6506c47da3d7461f4d2e0559a4
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-11-02 02:34:53 -0300

    Fix landing page responsivity

--- src/components/pages/LandingPage.js
+++ src/components/pages/LandingPage.js
@@ -2,23 +2,22 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import RegisterForm from '../forms/RegisterForm'
-import './LandingPage.scss'
 
 const LandingPage = () => (
-  <div className="landing__jumbo">
+  <div className="bg-primary c-on-primary flex flex-column flex-row-l pa3 justify-around items-center vh-100 w-100">
     <div>
-      <h1 className="mdc-typography--headline2 landing__app-title">
+      <h1 className="mdc-typography--headline2 b">
         Cramkle
       </h1>
-      <h2 className="mdc-typography--headline4 landing__moto">
+      <h2 className="mdc-typography--headline4 mv4 mv0">
         Make sure your knowledge will last
       </h2>
     </div>
-    <div className="landing__register-container">
+    <div className="inline-flex flex-column items-center w-100" style={{ maxWidth: 450 }}>
       <RegisterForm title="Sign up now!" />
 
-      <span className="landing__login-text">
-        Already have an account? <Link to="/login">Log In</Link>
+      <span className="landing__login-text mt3">
+        Already have an account? <Link to="/login" className="b c-on-primary">Log In</Link>
       </span>
     </div>
   </div>
