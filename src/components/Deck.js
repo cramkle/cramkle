61bb00e10ea397c42f9ae545d50912d6e1404b32
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-11-02 19:49:13 -0300

    Use link component instead of withRouter

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import { withRouter } from 'react-router'
+import { Link } from 'react-router-dom'
 import Card, { CardPrimaryContent } from '@material/react-card'
 
 const Deck = ({ title, description, slug, history }) => (
   <Card outlined>
     <CardPrimaryContent className="pa2">
-      <div onClick={() => history.push(`/d/${slug}`)}>
+      <Link className="link c-on-surface" to={`/d/${slug}`}>
         <h3 className="mdc-typography--headline6">{title}</h3>
         <p className="mdc-typography--body2">{description}</p>
-      </div>
+      </Link>
     </CardPrimaryContent>
   </Card>
 )
@@ -17,4 +17,4 @@ Deck.defaultProps = {
   description: null,
 }
 
-export default withRouter(Deck)
+export default Deck
