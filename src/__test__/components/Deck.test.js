bed6ce64b80eaab9517bead9f016b600a376de04
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-11-02 19:49:27 -0300

    Fix tests and update snapshot

--- src/__test__/components/Deck.test.js
+++ src/__test__/components/Deck.test.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import { MemoryRouter } from 'react-router'
 import { render } from 'react-testing-library'
 
 import Deck from '../../components/Deck'
 
 describe('<Deck />', () => {
   it('should match snapshot', () => {
-    const comp = render(<Deck title="日本語げんき" />)
+    const comp = render(
+      <MemoryRouter>
+        <Deck title="日本語げんき" />
+      </MemoryRouter>
+    )
 
     expect(comp.container).toMatchSnapshot()
   })
