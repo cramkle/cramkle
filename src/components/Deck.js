11d87a40600c1b798606cb094955ce7fd3b5a276
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-11-02 21:04:39 -0300

    Fix height of clickable content

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -4,7 +4,7 @@ import Card, { CardPrimaryContent } from '@material/react-card'
 
 const Deck = ({ title, description, slug, history }) => (
   <Card outlined className="h-100">
-    <CardPrimaryContent className="pa2">
+    <CardPrimaryContent className="pa2 h-100">
       <Link className="link c-on-surface" to={`/d/${slug}`}>
         <h3 className="mdc-typography--headline6">{title}</h3>
         <p className="mdc-typography--body2">{description}</p>
