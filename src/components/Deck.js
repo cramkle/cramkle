dae10813786e4b92f5c8fe5b55ec49c71633369f
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-11-16 19:01:22 -0300

    Remove link from deck card and fix lint

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router'
 import Card, { CardPrimaryContent } from '@material/react-card'
 
 const Deck = ({ title, description, slug, history }) => (
   <Card outlined className="h-100">
-    <CardPrimaryContent className="pa2 h-100">
-      <Link className="link c-on-surface" to={`/d/${slug}`}>
-        <h3 className="mdc-typography--headline6">{title}</h3>
-        <p className="mdc-typography--body2">{description}</p>
-      </Link>
+    <CardPrimaryContent
+      className="pa2 h-100"
+      onClick={() => history.push(`/d/${slug}`)}
+    >
+      <h3 className="mdc-typography--headline6">{title}</h3>
+      <p className="mdc-typography--body2">{description}</p>
     </CardPrimaryContent>
   </Card>
 )
@@ -17,4 +18,4 @@ Deck.defaultProps = {
   description: null,
 }
 
-export default Deck
+export default withRouter(Deck)
--- src/components/routes/TopBarRoute.js
+++ src/components/routes/TopBarRoute.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-import { RouteProps } from 'react-router'
 
 import TopBar from '../TopBar'
 
