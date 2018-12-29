a1647f5fcb80daf77db812a8b0b0f5927b41087a
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-12-28 22:18:30 -0300

    Move webpack configuration to single file

--- scripts/build.js
+++ scripts/build.js
@@ -16,8 +16,8 @@ const chalk = require('chalk')
 const fs = require('fs-extra')
 const webpack = require('webpack')
 const bfj = require('bfj')
-const clientConfig = require('../config/webpack.config.prod')
-const serverConfig = require('../config/webpack.config.server')
+
+const getBaseWebpackConfig = require('../config/createWebpackConfig')
 const paths = require('../config/paths')
 const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles')
 const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages')
@@ -108,7 +108,11 @@ checkBrowsers(paths.appPath, isInteractive)
 function build(previousFileSizes) {
   console.log('Creating an optimized production build...')
 
-  let compiler = webpack([clientConfig, serverConfig])
+  let compiler = webpack([
+    getBaseWebpackConfig(),
+    getBaseWebpackConfig({ isServer: true }),
+  ])
+
   return new Promise((resolve, reject) => {
     compiler.run((err, stats) => {
       let messages
