03978f63cc4fabcb0d8483355cbd3accfdaf2dde
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-12-30 18:48:08 -0300

    Add cache group for css

--- config/createWebpackConfig.js
+++ config/createWebpackConfig.js
@@ -90,6 +90,12 @@ const optimizationConfig = ({ dev, isServer }) => {
       cacheGroups: {
         default: false,
         vendors: false,
+        styles: {
+          name: 'styles',
+          test: /.(sa|sc|c)ss$/,
+          chunks: 'all',
+          enforce: true,
+        },
       },
     },
   }
