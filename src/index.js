757e33ddcddc04e283de948f7bd61734e0e1c9d1
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-02-06 19:55:12 -0200

    Update call to getDataFromTree

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import client from './utils/apolloClient'
 import App from './App'
 
 const renderWithData = rootComponent => {
+  // We use dynamic import here to avoid placing these
+  // dependencies in the client bundle.
   return Promise.all([
-    // We use dynamic import here to avoid placing these
-    // dependencies in the client bundle.
     import('react-apollo'),
     import('react-dom/server'),
     import('react-router-dom'),
@@ -23,14 +23,14 @@ const renderWithData = rootComponent => {
       </StaticRouter>
     )
 
-    // TODO: remember to change to `getMarkupFromTree` with the new release of react-apollo
-    return getDataFromTree(
-      root,
-      renderToString
-    ).then(markup => ({
-      markup,
-      routerContext,
-    }))
+    return getDataFromTree(root).then(() => {
+      const markup = renderToString(root)
+
+      return {
+        markup,
+        routerContext,
+      }
+    })
   })
 }
 
@@ -40,7 +40,9 @@ const render = () => {
   if (canUseDOM) {
     const elem = document.getElementById('root')
 
-    createRoot(elem, { hydrate: true }).render(<BrowserRouter>{root}</BrowserRouter>)
+    createRoot(elem, { hydrate: true }).render(
+      <BrowserRouter>{root}</BrowserRouter>
+    )
   } else {
     return renderWithData(root)
   }
@@ -60,10 +62,11 @@ const start = () => {
     // The dynamic import is used here because the code inside `registerServiceWorker`
     // needs access to the `window` object, and since we don't have it on the server,
     // it would just break the build.
-    import('./registerServiceWorker')
-      .then(({ default: registerServiceWorker }) => {
+    import('./registerServiceWorker').then(
+      ({ default: registerServiceWorker }) => {
         registerServiceWorker()
-      })
+      }
+    )
   }
 }
 
