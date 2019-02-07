3144e05db0b3e0a61dd7231447e47aaa8d38d31d
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-02-06 23:18:53 -0200

    Remove appBuild path and add clientStatic

--- config/constants.js
+++ config/constants.js
@@ -1,10 +1,12 @@
-const STATIC_FOLDER = 'static'
-const STATIC_RUNTIME_PATH = `${STATIC_FOLDER}/runtime`
-const STATIC_CHUNKS_PATH = `${STATIC_FOLDER}/chunks`
-const STATIC_MEDIA_PATH = `${STATIC_FOLDER}/media`
+const path = require('path')
 
-const STATIC_RUNTIME_MAIN = `${STATIC_RUNTIME_PATH}/main`
-const STATIC_RUNTIME_WEBPACK = `${STATIC_RUNTIME_PATH}/webpack`
+const STATIC_FOLDER = 'static'
+const STATIC_RUNTIME_PATH = path.join(STATIC_FOLDER, 'runtime')
+const STATIC_CHUNKS_PATH = path.join(STATIC_FOLDER, 'chunks')
+const STATIC_MEDIA_PATH = path.join(STATIC_FOLDER, 'media')
+
+const STATIC_RUNTIME_MAIN = path.join(STATIC_RUNTIME_PATH, 'main')
+const STATIC_RUNTIME_WEBPACK = path.join(STATIC_RUNTIME_PATH, 'webpack')
 
 module.exports = {
   STATIC_FOLDER,
