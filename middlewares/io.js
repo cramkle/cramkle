d1aeec878a07179fc6f3a3667afe1b2ae17aebd5
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-02-14 01:09:26 -0200

    Add cookie parser

--- middlewares/io.js
+++ middlewares/io.js
@@ -1,9 +1,11 @@
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-paser')
 const cors = require('cors')
 
 module.exports = {
   set: app => {
     app.use(cors())
+    app.use(cookieParser())
     app.use(bodyParser.json())
   },
 }
