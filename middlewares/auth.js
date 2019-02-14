1e18ffce6a512a4eda3083c94f150fefd3aef6eb
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-02-14 01:43:37 -0200

    Add serialize methods

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,7 +9,7 @@ passport.use(new Strategy(
     let user = null
 
     try {
-      user = await User.findOne({ username }).exec()
+      user = await User.findOne({ username }).lean().exec()
     } catch (e) {
       done(e)
       return
@@ -29,9 +29,30 @@ passport.use(new Strategy(
   }
 ))
 
+passport.serializeUser((user, done) => {
+  done(null, user._id)
+})
+
+passport.deserializeUser(async (id, done) => {
+  let user = null
+
+  try {
+    user = await User.findOne({ _id: id }).lean().exec()
+  } catch (e) {
+    done(e)
+    return
+  }
+
+  done(null, user)
+})
+
 module.exports = {
   set: app => {
-    app.use(session())
+    app.use(session({
+      secret: 'hello world',
+      resave: false,
+      saveUninitialized: true,
+    }))
     app.use(passport.initialize())
     app.use(passport.session())
   },
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,12 @@ const passport = require('passport')
 
 const router = express.Router()
 
-router.post('/login', passport.authenticate('local'))
+router.post(
+  '/login',
+  passport.authenticate('local'),
+  (req, res) => {
+    res.json({ id: req.user._id, username: req.user.username })
+  }
+)
 
 module.exports = router
