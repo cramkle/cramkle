1c5d6a41206aad5de07d1fa20bbadd678f25b71b
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-02-14 12:33:07 -0200

    Add logout route

--- routes/auth.js
+++ routes/auth.js
@@ -7,4 +7,9 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
   res.json({ id: req.user._id, username: req.user.username })
 })
 
+router.post('/logout', (req, res) => {
+  req.logout()
+  res.json({ ok: true })
+})
+
 module.exports = router
