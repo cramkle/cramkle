8afe95b02a84a9a69bc59df378c7e5229997784c
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-02-14 01:33:13 -0200

    Add missing returns

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -12,14 +12,17 @@ passport.use(new Strategy(
       user = await User.findOne({ username }).exec()
     } catch (e) {
       done(e)
+      return
     }
 
     if (!user) {
       done(null, false, { message: 'Incorrect username' })
+      return
     }
 
     if (!await user.comparePassword(password, user.password)) {
       done(null, false, { message: 'Incorrect password' })
+      return
     }
 
     return done(null, user)
