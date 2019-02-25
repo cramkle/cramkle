96e0e7c6a7c363db2ac99a0a7af7208487cc2363
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-02-25 19:22:14 -0300

    Fix import typo

--- resolvers/note/index.ts
+++ resolvers/note/index.ts
@@ -1,11 +1,11 @@
-import { Note, Deck, CardModel, FieldValues, Card } from '../../models'
+import { Note, Deck, CardModel, FieldValue, Card } from '../../models'
 
 export const root = {
   Note: {
     id: root => root._id.toString(),
     deck: root => Deck.findById(root.deckId),
     model: root => CardModel.findById(root.modelId),
-    values: root => Promise.all(root.values.map(FieldValues.findById)),
+    values: root => Promise.all(root.values.map(FieldValue.findById)),
     cards: root => Promise.all(root.cards.map(Card.findById)),
   },
 }
