6587e954733ea2df3c7383fe2167dc43f37a8e9a
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-03-02 16:42:49 -0300

    Add form with create deck mutation

--- src/components/forms/AddDeckForm.tsx
+++ src/components/forms/AddDeckForm.tsx
@@ -1,18 +1,90 @@
-import React from 'react'
+import { Formik } from 'formik'
+import { isEmpty } from 'ramda'
+import { graphql, ChildMutateProps } from 'react-apollo'
+import React, { useCallback, useRef } from 'react'
+import * as yup from 'yup'
+import Dialog, {
+  DialogContent,
+  DialogTitle,
+  DialogFooter,
+  DialogButton,
+} from '@material/react-dialog'
 
 import InputField from './InputField'
+import createDeckMutation from '../../graphql/createDeckMutation.gql'
 
 interface Props {
-  onSubmit: () => void
+  open: boolean
+  onClose: () => void
 }
 
-const AddDeckForm: React.FunctionComponent<Props> = ({ onSubmit }) => {
+const AddDeckForm: React.FunctionComponent<ChildMutateProps<Props>> = ({
+  open,
+  onClose,
+  mutate,
+}) => {
+  const formRef = useRef<Formik>(null)
+
+  const handleClose = useCallback(
+    (action: string) => {
+      return new Promise((resolve, reject) => {
+        if (action === 'create') {
+          return formRef.current.runValidations().then(errors => {
+            if (isEmpty(errors)) {
+              return formRef.current.submitForm()
+            } else {
+              return reject('Form submit with errors')
+            }
+          })
+        } else {
+          resolve()
+        }
+      }).then(onClose)
+    },
+    [onClose, formRef.current]
+  )
+
   return (
-    <form onSubmit={onSubmit}>
-      <InputField name="title" label="Title" />
-      <InputField name="description" label="Description" />
-    </form>
+    <Formik
+      initialValues={{
+        title: '',
+        description: '',
+      }}
+      validationSchema={yup.object().shape({
+        title: yup.string().required('The title is required'),
+        description: yup.string(),
+      })}
+      onSubmit={({ title, description }) => {
+        console.log('on submit')
+        mutate({ variables: { title, description } }).then(() => {
+          onClose()
+        })
+      }}
+      isInitialValid={false}
+      ref={formRef}
+    >
+      {({ handleSubmit }) => (
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Add deck</DialogTitle>
+          <DialogContent>
+            <form className="w-100" onSubmit={handleSubmit}>
+              <InputField className="w-100" name="title" label="Title" />
+              <InputField
+                className="w-100 mt3"
+                name="description"
+                label="Description"
+              />
+            </form>
+          </DialogContent>
+          <DialogFooter>
+            <DialogButton action="create" isDefault>
+              Create
+            </DialogButton>
+          </DialogFooter>
+        </Dialog>
+      )}
+    </Formik>
   )
 }
 
-export default AddDeckForm
+export default graphql<Props>(createDeckMutation)(AddDeckForm)
--- src/components/pages/DashboardPage.tsx
+++ src/components/pages/DashboardPage.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState, useCallback } from 'react'
 import { Helmet } from 'react-helmet'
 
 import TabBar from '@material/react-tab-bar'
@@ -7,9 +7,24 @@ import MaterialIcon from '@material/react-material-icon'
 import Fab from '@material/react-fab'
 
 import DeckList from '../DeckList'
+import AddDeckForm from '../forms/AddDeckForm'
 
 const DashboardPage: React.FunctionComponent = () => {
   const [index, setIndex] = useState(0)
+  const [dialogOpen, setDialogOpen] = useState(false)
+
+  const handleDialogClose = useCallback(() => {
+    setDialogOpen(false)
+  }, [])
+
+  const handleDialogOpen = useCallback(() => {
+    setDialogOpen(true)
+  }, [])
+
+  const handleActiveIndexUpdate = useCallback(index => {
+    setIndex(index)
+  }, [])
+
   return (
     <Fragment>
       <Helmet>
@@ -18,7 +33,7 @@ const DashboardPage: React.FunctionComponent = () => {
 
       <TabBar
         activeIndex={index}
-        handleActiveIndexUpdate={index => setIndex(index)}
+        handleActiveIndexUpdate={handleActiveIndexUpdate}
       >
         <Tab>Study</Tab>
         <Tab>Decks</Tab>
@@ -26,8 +41,14 @@ const DashboardPage: React.FunctionComponent = () => {
 
       <DeckList />
 
+      <AddDeckForm open={dialogOpen} onClose={handleDialogClose} />
+
       <div className="absolute right-0 bottom-0 pa4">
-        <Fab icon={<MaterialIcon icon="add" />} textLabel="Add Deck" />
+        <Fab
+          icon={<MaterialIcon icon="add" />}
+          textLabel="Add Deck"
+          onClick={handleDialogOpen}
+        />
       </div>
     </Fragment>
   )
