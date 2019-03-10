994f0d7c61a42aab520ab0e7fa42c2b6a3a2587e
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-03-10 20:44:08 -0300

    Convert logs to errors

--- src/utils/apolloClient.ts
+++ src/utils/apolloClient.ts
@@ -13,13 +13,13 @@ export const createApolloClient = (uri: string) => {
   const errorLink = onError(({ graphQLErrors, networkError }) => {
     if (graphQLErrors) {
       graphQLErrors.forEach(({ message, locations, path }) =>
-        console.log(
+        console.error(
           `[GraphQL error]: Message ${message}, Location: ${locations}, Path ${path}`
         )
       )
     }
     if (networkError) {
-      console.log(`[Network error]: ${networkError}`)
+      console.error(`[Network error]: ${networkError}`)
     }
   })
 
