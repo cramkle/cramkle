1591376f12de5ea40eda916a418eeb8fd45c5ac6
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-03-10 20:45:00 -0300

    Fix ts-ignore comment

--- src/components/forms/AddDeckForm.tsx
+++ src/components/forms/AddDeckForm.tsx
@@ -1,13 +1,13 @@
-import { Formik, Field } from 'formik'
-import { graphql, ChildMutateProps } from 'react-apollo'
-import React, { useRef } from 'react'
-import * as yup from 'yup'
 import Dialog, {
   DialogContent,
   DialogTitle,
   DialogFooter,
   DialogButton,
 } from '@material/react-dialog'
+import { Formik, Field } from 'formik'
+import { graphql, ChildMutateProps } from 'react-apollo'
+import React from 'react'
+import * as yup from 'yup'
 
 import { IDeck } from '../../types/Deck'
 import decksQuery from '../../graphql/decksQuery.gql'
@@ -29,87 +29,69 @@ interface QueryData {
 
 const AddDeckForm: React.FunctionComponent<
   ChildMutateProps<Props, MutationData>
-> = ({ open, onClose, mutate }) => {
-  const submittingRef = useRef(false)
+> = ({ open, onClose, mutate }) => (
+  <Formik
+    initialValues={{
+      title: '',
+      description: '',
+    }}
+    validationSchema={yup.object().shape({
+      title: yup.string().required('The title is required'),
+      description: yup.string(),
+    })}
+    onSubmit={(values, props) => {
+      mutate({
+        variables: values,
+        update: (proxy, { data: { createDeck } }) => {
+          const data = proxy.readQuery<QueryData>({ query: decksQuery })
 
-  return (
-    <Formik
-      initialValues={{
-        title: '',
-        description: '',
-      }}
-      validationSchema={yup.object().shape({
-        title: yup.string().required('The title is required'),
-        description: yup.string(),
-      })}
-      onSubmit={(values, props) => {
-        // avoid double submits on enter keypress
-        if (submittingRef.current) {
-          return
-        }
+          data.decks.push(createDeck)
 
-        submittingRef.current = true
-
-        mutate({
-          variables: values,
-          update: (proxy, { data: { createDeck } }) => {
-            const data = proxy.readQuery<QueryData>({ query: decksQuery })
-
-            data.decks.push(createDeck)
-
-            proxy.writeQuery({ query: decksQuery, data })
-          },
+          proxy.writeQuery({ query: decksQuery, data })
+        },
+      })
+        .then(() => {
+          props.resetForm()
+          onClose('created')
         })
-          .then(() => {
-            props.resetForm()
-            submittingRef.current = false
-            onClose('created')
-          })
-          .finally(() => {
-            props.setSubmitting(false)
-          })
-      }}
-      isInitialValid={false}
-    >
-      {({ handleSubmit }) => (
-        <Dialog
-          tag="form"
-          scrimClickAction="dismiss"
-          open={open}
-          onSubmit={handleSubmit}
-          onClose={onClose}
-        >
-          <DialogTitle>Add deck</DialogTitle>
-          <DialogContent style={{ width: '320px' }}>
-            <Field
-              className="w-100"
-              name="title"
-              label="Title"
-              component={TextInput}
-            />
-            <Field
-              component={TextInput}
-              className="w-100 mt3"
-              name="description"
-              label="Description"
-              textarea
-            />
-          </DialogContent>
-          <DialogFooter>
-            <DialogButton
-              action="create"
-              isDefault
-              // @ts-ignore
-              onClick={handleSubmit}
-              type="submit"
-            >
-              Create
-            </DialogButton>
-          </DialogFooter>
-        </Dialog>
-      )}
-    </Formik>
-  )
-}
+        .finally(() => {
+          props.setSubmitting(false)
+        })
+    }}
+    isInitialValid={false}
+  >
+    {({ handleSubmit }) => (
+      <Dialog
+        tag="form"
+        scrimClickAction="dismiss"
+        open={open}
+        onSubmit={handleSubmit}
+        onClose={onClose}
+      >
+        <DialogTitle>Add deck</DialogTitle>
+        <DialogContent style={{ width: '320px' }}>
+          <Field
+            className="w-100"
+            name="title"
+            label="Title"
+            component={TextInput}
+          />
+          <Field
+            component={TextInput}
+            className="w-100 mt3"
+            name="description"
+            label="Description"
+            textarea
+          />
+        </DialogContent>
+        <DialogFooter>
+          <DialogButton action="create" isDefault type="submit">
+            Create
+          </DialogButton>
+        </DialogFooter>
+      </Dialog>
+    )}
+  </Formik>
+)
 
 export default graphql<Props, MutationData>(createDeckMutation)(AddDeckForm)
