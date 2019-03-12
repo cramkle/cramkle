afc61e49da29c2bfd32e96851be06e45bf190eae
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-03-12 19:48:45 -0300

    Update drawer with mobile flag

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState } from 'react'
 import { withRouter, RouteComponentProps } from 'react-router'
 import { graphql, ChildDataProps } from 'react-apollo'
 import TopAppBar from '@material/react-top-app-bar'
@@ -11,6 +11,7 @@ import List, {
   ListItemGraphic,
 } from '@material/react-list'
 
+import { useMobile } from '../hooks/useMobile'
 import loadingQuery from '../graphql/topBarLoadingQuery.gql'
 
 interface Data {
@@ -28,72 +29,101 @@ const TopBar: React.FunctionComponent<
     topBar: { loading },
   },
 }) => {
+  const isMobile = useMobile()
+
   const [drawerOpen, setDrawerOpen] = useState(false)
 
-  const handleLogout = useCallback(() => {
+  const handleLogout = () => {
     fetch('/_c/auth/logout', {
       method: 'POST',
       credentials: 'include',
     }).then(() => window.location.assign('/login'))
-  }, [])
+  }
 
-  const handleTopBarIconClick = useCallback(() => {
-    setDrawerOpen(!drawerOpen)
-  }, [drawerOpen])
+  const handleTopBarIconClick = () => {
+    setDrawerOpen(isOpen => !isOpen)
+  }
 
-  const handleDrawerClose = useCallback(() => {
+  const handleDrawerClose = () => {
     setDrawerOpen(false)
-  }, [])
+  }
 
   if (React.Children.count(children) === 0) {
     return null
   }
 
-  return (
-    <div className="vh-100 flex">
-      <Drawer open={drawerOpen} onClose={handleDrawerClose} modal>
-        <DrawerContent>
-          <List>
-            <ListItem tabIndex={0} onClick={() => history.push('/')}>
-              <ListItemGraphic graphic={<MaterialIcon icon="home" />} />
-              <ListItemText primaryText="Home" />
-            </ListItem>
-            <ListItem onClick={() => history.push('/marketplace')}>
-              <ListItemGraphic
-                graphic={<MaterialIcon icon="store_mall_directory" />}
-              />
-              <ListItemText primaryText="Marketplace" />
-            </ListItem>
-            <ListItem onClick={() => history.push('/statistics')}>
-              <ListItemGraphic graphic={<MaterialIcon icon="bar_chart" />} />
-              <ListItemText primaryText="Statistics" />
-            </ListItem>
-            <ListItem onClick={() => history.push('/settings')}>
-              <ListItemGraphic graphic={<MaterialIcon icon="settings" />} />
-              <ListItemText primaryText="Settings" />
-            </ListItem>
-            <ListItem onClick={handleLogout}>
-              <ListItemGraphic graphic={<MaterialIcon icon="exit_to_app" />} />
-              <ListItemText primaryText="Logout" />
-            </ListItem>
-          </List>
-        </DrawerContent>
-      </Drawer>
-      <DrawerAppContent className="w-100">
-        <TopAppBar
-          title="Cramkle"
-          className="absolute left-0 right-0"
-          navigationIcon={
-            <MaterialIcon icon="menu" onClick={handleTopBarIconClick} />
-          }
+  const drawerItems = (
+    <List>
+      <ListItem tabIndex={0} onClick={() => history.push('/')}>
+        <ListItemGraphic graphic={<MaterialIcon icon="home" />} />
+        <ListItemText primaryText="Home" />
+      </ListItem>
+      <ListItem onClick={() => history.push('/marketplace')}>
+        <ListItemGraphic
+          graphic={<MaterialIcon icon="store_mall_directory" />}
         />
-        <div className="mdc-top-app-bar--fixed-adjust w-100 h-100">
-          {loading && <LinearProgress indeterminate />}
-          <div className="h-100 overflow-auto">{children}</div>
-        </div>
-      </DrawerAppContent>
-    </div>
+        <ListItemText primaryText="Marketplace" />
+      </ListItem>
+      <ListItem onClick={() => history.push('/statistics')}>
+        <ListItemGraphic graphic={<MaterialIcon icon="bar_chart" />} />
+        <ListItemText primaryText="Statistics" />
+      </ListItem>
+      <ListItem onClick={() => history.push('/settings')}>
+        <ListItemGraphic graphic={<MaterialIcon icon="settings" />} />
+        <ListItemText primaryText="Settings" />
+      </ListItem>
+      <ListItem onClick={handleLogout}>
+        <ListItemGraphic graphic={<MaterialIcon icon="exit_to_app" />} />
+        <ListItemText primaryText="Logout" />
+      </ListItem>
+    </List>
   )
+
+  let content = (
+    <>
+      {loading && <LinearProgress indeterminate />}
+      <div className="h-100 overflow-auto w-100">{children}</div>
+    </>
+  )
+
+  if (!isMobile) {
+    content = (
+      <>
+        <Drawer open={drawerOpen} onClose={handleDrawerClose}>
+          <DrawerContent>{drawerItems}</DrawerContent>
+        </Drawer>
+        <DrawerAppContent className="w-100">{content}</DrawerAppContent>
+      </>
+    )
+  }
+
+  let wrapper = (
+    <>
+      <TopAppBar
+        title="Cramkle"
+        className="absolute left-0 right-0"
+        navigationIcon={
+          <MaterialIcon icon="menu" onClick={handleTopBarIconClick} />
+        }
+      />
+      <div className="mdc-top-app-bar--fixed-adjust w-100 h-100 flex">
+        {content}
+      </div>
+    </>
+  )
+
+  if (isMobile) {
+    wrapper = (
+      <>
+        <Drawer open={drawerOpen} onClose={handleDrawerClose} modal>
+          <DrawerContent>{drawerItems}</DrawerContent>
+        </Drawer>
+        <DrawerAppContent className="w-100">{wrapper}</DrawerAppContent>
+      </>
+    )
+  }
+
+  return <div className="vh-100 flex">{wrapper}</div>
 }
 
 export default withRouter(
