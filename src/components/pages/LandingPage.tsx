9c3e89162d339c22bad10c5d6d21d30bacaba834
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-03-16 19:24:44 -0300

    Update landing page

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -1,31 +1,61 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import TopAppBar, {
+  TopAppBarFixedAdjust,
+  TopAppBarIcon,
+  TopAppBarRow,
+  TopAppBarSection,
+  TopAppBarTitle,
+} from '@material/react-top-app-bar'
 import { Headline2, Headline4 } from '@material/react-typography'
 
+import logoUrl from '../../assets/logo.svg'
 import RegisterForm from '../forms/RegisterForm'
 
 const LandingPage: React.FunctionComponent = () => (
-  <div className="bg-primary c-on-primary flex flex-column flex-row-l pa3 justify-around items-center vh-100 w-100">
-    <div>
-      <Headline2 className="b">Cramkle</Headline2>
-      <Headline4 className="mv4 mv0">
-        Make sure your knowledge will last
-      </Headline4>
-    </div>
-    <div
-      className="inline-flex flex-column items-center w-100"
-      style={{ maxWidth: 450 }}
-    >
-      <RegisterForm title="Sign up now!" />
+  <>
+    <TopAppBar className="landing-bar" fixed>
+      <TopAppBarRow>
+        <TopAppBarSection align="start">
+          <TopAppBarIcon>
+            <img style={{ height: 24, width: 24 }} src={logoUrl} alt="" />
+          </TopAppBarIcon>
+          <TopAppBarTitle>Cramkle</TopAppBarTitle>
+        </TopAppBarSection>
+      </TopAppBarRow>
+    </TopAppBar>
+    <TopAppBarFixedAdjust>
+      <div className="bg-primary c-on-primary vh-100 w-100">
+        <div className="flex flex-column flex-row-ns justify-around items-center mh2 mh5-l pv2 pv6-ns">
+          <div
+            className="flex flex-wrap pv4 pv0-ns w-100"
+            style={{ maxWidth: 450 }}
+          >
+            <Headline2 className="f2 f1-ns b lh-title">
+              Make sure your knowledge will last
+            </Headline2>
+            <Headline4 className="f5 f3-ns mt3 lh-title">
+              Optimize your knowledge retention with this effective study
+              method.
+            </Headline4>
+          </div>
+          <div
+            className="inline-flex flex-column items-center w-100"
+            style={{ maxWidth: 450 }}
+          >
+            <RegisterForm title="Sign up now!" />
 
-      <span className="landing__login-text mt3">
-        Already have an account?{' '}
-        <Link to="/login" className="b c-on-primary">
-          Log In
-        </Link>
-      </span>
-    </div>
-  </div>
+            <span className="landing__login-text mt3">
+              Already have an account?{' '}
+              <Link to="/login" className="b c-on-primary">
+                Log In
+              </Link>
+            </span>
+          </div>
+        </div>
+      </div>
+    </TopAppBarFixedAdjust>
+  </>
 )
 
 export default LandingPage
