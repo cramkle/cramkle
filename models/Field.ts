6bddcb2fa36f172735e0a512838c7d933ed1f6eb
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-03-17 14:45:22 -0300

    Remove type and isRequired from field model

--- models/Field.ts
+++ models/Field.ts
@@ -2,19 +2,12 @@ import { Schema, model, Document } from 'mongoose'
 
 interface Field {
   name: string
-  isRequired: boolean
-  type: 'STRING' | 'NUMBER' | 'DATE' | 'IMAGE'
 }
 
 interface FieldDocument extends Field, Document {}
 
 const FieldSchema = new Schema<FieldDocument>({
   name: { type: String, required: true },
-  isRequired: Boolean,
-  type: {
-    type: String,
-    enum: ['STRING', 'NUMBER', 'DATE', 'IMAGE'],
-  },
   modelId: {
     type: Schema.Types.ObjectId,
     ref: 'CardModel',
