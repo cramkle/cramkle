e6f9dec6b0406abce6f992c121cb0720aca6d251
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-03-30 18:09:45 -0300

    Extract drawer logic to separate component

---
+++ src/components/AppDrawer.tsx
@@ -0,0 +1,98 @@
+import Drawer, { DrawerContent, DrawerAppContent } from '@material/react-drawer'
+import List, {
+  ListItem,
+  ListItemText,
+  ListItemGraphic,
+} from '@material/react-list'
+import MaterialIcon from '@material/react-material-icon'
+import React, { ReactNode, useRef, useLayoutEffect } from 'react'
+import { withRouter, RouteComponentProps } from 'react-router'
+
+import useWindowSize from '../hooks/useWindowSize'
+import { useMobile } from '../hooks/useMobile'
+
+interface Props extends RouteComponentProps {
+  content: ReactNode
+  render: (content: ReactNode) => JSX.Element
+  open: boolean
+  onClose: () => void
+}
+
+const AppDrawer: React.FunctionComponent<Props> = ({
+  content,
+  render,
+  open,
+  onClose,
+  history,
+}) => {
+  const isMobile = useMobile()
+  const { height: windowHeight } = useWindowSize()
+
+  const drawerRef = useRef<HTMLElement>(null)
+
+  const handleLogout = () => {
+    fetch('/_c/auth/logout', {
+      method: 'POST',
+      credentials: 'include',
+    }).then(() => window.location.assign('/login'))
+  }
+
+  useLayoutEffect(() => {
+    if (isMobile || drawerRef.current === null) {
+      return
+    }
+
+    drawerRef.current.style.height = `${windowHeight - 48}px`
+  }, [isMobile, windowHeight])
+
+  const drawerItems = (
+    <List>
+      <ListItem tabIndex={0} onClick={() => history.push('/')}>
+        <ListItemGraphic graphic={<MaterialIcon icon="home" />} />
+        <ListItemText primaryText="Home" />
+      </ListItem>
+      <ListItem onClick={() => history.push('/marketplace')}>
+        <ListItemGraphic
+          graphic={<MaterialIcon icon="store_mall_directory" />}
+        />
+        <ListItemText primaryText="Marketplace" />
+      </ListItem>
+      <ListItem onClick={() => history.push('/statistics')}>
+        <ListItemGraphic graphic={<MaterialIcon icon="bar_chart" />} />
+        <ListItemText primaryText="Statistics" />
+      </ListItem>
+      <ListItem onClick={() => history.push('/settings')}>
+        <ListItemGraphic graphic={<MaterialIcon icon="settings" />} />
+        <ListItemText primaryText="Settings" />
+      </ListItem>
+      <ListItem onClick={handleLogout}>
+        <ListItemGraphic graphic={<MaterialIcon icon="exit_to_app" />} />
+        <ListItemText primaryText="Logout" />
+      </ListItem>
+    </List>
+  )
+
+  if (isMobile) {
+    return (
+      <>
+        <Drawer open={open} onClose={onClose} modal>
+          <DrawerContent>{drawerItems}</DrawerContent>
+        </Drawer>
+        <DrawerAppContent className="w-100">{render(content)}</DrawerAppContent>
+      </>
+    )
+  }
+
+  return render(
+    <>
+      <Drawer open={open} dismissible innerRef={drawerRef}>
+        <DrawerContent>{drawerItems}</DrawerContent>
+      </Drawer>
+      <DrawerAppContent className="w-100 flex overflow-auto">
+        {content}
+      </DrawerAppContent>
+    </>
+  )
+}
+
+export default withRouter(AppDrawer)
--- src/components/Shell.tsx
+++ src/components/Shell.tsx
@@ -1,11 +1,4 @@
-import React, {
-  Suspense,
-  useState,
-  useEffect,
-  useRef,
-  useLayoutEffect,
-} from 'react'
-import { withRouter, RouteComponentProps } from 'react-router'
+import React, { Suspense, useState, useEffect } from 'react'
 import { graphql, ChildDataProps } from 'react-apollo'
 import TopAppBar, {
   TopAppBarFixedAdjust,
@@ -16,15 +9,9 @@ import TopAppBar, {
 } from '@material/react-top-app-bar'
 import MaterialIcon from '@material/react-material-icon'
 import LinearProgress from '@material/react-linear-progress'
-import Drawer, { DrawerContent, DrawerAppContent } from '@material/react-drawer'
-import List, {
-  ListItem,
-  ListItemText,
-  ListItemGraphic,
-} from '@material/react-list'
 
+import AppDrawer from './AppDrawer'
 import NoSSR from './NoSSR'
-import useWindowSize from '../hooks/useWindowSize'
 import { useMobile } from '../hooks/useMobile'
 import loadingQuery from '../graphql/topBarLoadingQuery.gql'
 
@@ -34,40 +21,18 @@ interface Data {
   }
 }
 
-const Shell: React.FunctionComponent<
-  ChildDataProps<RouteComponentProps, Data>
-> = ({
+const Shell: React.FunctionComponent<ChildDataProps<{}, Data>> = ({
   children,
-  history,
   data: {
     topBar: { loading },
   },
 }) => {
   const isMobile = useMobile()
-  const { height: windowHeight } = useWindowSize()
-
-  const drawerRef = useRef<HTMLElement>(null)
-
   const [drawerOpen, setDrawerOpen] = useState(false)
 
   useEffect(() => {
     setDrawerOpen(!isMobile)
   }, [isMobile])
-
-  useLayoutEffect(() => {
-    if (isMobile || drawerRef.current === null) {
-      return
-    }
-
-    drawerRef.current.style.height = `${windowHeight - 48}px`
-  }, [isMobile, windowHeight])
-
-  const handleLogout = () => {
-    fetch('/_c/auth/logout', {
-      method: 'POST',
-      credentials: 'include',
-    }).then(() => window.location.assign('/login'))
-  }
 
   const handleNavigationIconClick = () => {
     setDrawerOpen(isOpen => !isOpen)
@@ -80,33 +45,6 @@ const Shell: React.FunctionComponent<
   if (React.Children.count(children) === 0) {
     return null
   }
-
-  const drawerItems = (
-    <List>
-      <ListItem tabIndex={0} onClick={() => history.push('/')}>
-        <ListItemGraphic graphic={<MaterialIcon icon="home" />} />
-        <ListItemText primaryText="Home" />
-      </ListItem>
-      <ListItem onClick={() => history.push('/marketplace')}>
-        <ListItemGraphic
-          graphic={<MaterialIcon icon="store_mall_directory" />}
-        />
-        <ListItemText primaryText="Marketplace" />
-      </ListItem>
-      <ListItem onClick={() => history.push('/statistics')}>
-        <ListItemGraphic graphic={<MaterialIcon icon="bar_chart" />} />
-        <ListItemText primaryText="Statistics" />
-      </ListItem>
-      <ListItem onClick={() => history.push('/settings')}>
-        <ListItemGraphic graphic={<MaterialIcon icon="settings" />} />
-        <ListItemText primaryText="Settings" />
-      </ListItem>
-      <ListItem onClick={handleLogout}>
-        <ListItemGraphic graphic={<MaterialIcon icon="exit_to_app" />} />
-        <ListItemText primaryText="Logout" />
-      </ListItem>
-    </List>
-  )
 
   const loader = (
     <LinearProgress
@@ -126,51 +64,35 @@ const Shell: React.FunctionComponent<
     </div>
   )
 
-  if (!isMobile) {
-    content = (
-      <>
-        <Drawer open={drawerOpen} dismissible innerRef={drawerRef}>
-          <DrawerContent>{drawerItems}</DrawerContent>
-        </Drawer>
-        <DrawerAppContent className="w-100 flex overflow-auto">
-          {content}
-        </DrawerAppContent>
-      </>
-    )
-  }
-
-  let wrapper = (
-    <>
-      <TopAppBar className="absolute left-0 right-0" fixed dense>
-        <TopAppBarRow>
-          <TopAppBarSection align="start">
-            <TopAppBarIcon navIcon tabIndex={0}>
-              <MaterialIcon icon="menu" onClick={handleNavigationIconClick} />
-            </TopAppBarIcon>
-            <TopAppBarTitle>Cramkle</TopAppBarTitle>
-          </TopAppBarSection>
-        </TopAppBarRow>
-      </TopAppBar>
-      <TopAppBarFixedAdjust className="w-100 flex relative" dense>
-        {content}
-      </TopAppBarFixedAdjust>
-    </>
+  return (
+    <div className="vh-100 flex">
+      <AppDrawer
+        open={drawerOpen}
+        onClose={handleDrawerClose}
+        content={content}
+        render={children => (
+          <>
+            <TopAppBar className="absolute left-0 right-0" fixed dense>
+              <TopAppBarRow>
+                <TopAppBarSection align="start">
+                  <TopAppBarIcon navIcon tabIndex={0}>
+                    <MaterialIcon
+                      icon="menu"
+                      onClick={handleNavigationIconClick}
+                    />
+                  </TopAppBarIcon>
+                  <TopAppBarTitle>Cramkle</TopAppBarTitle>
+                </TopAppBarSection>
+              </TopAppBarRow>
+            </TopAppBar>
+            <TopAppBarFixedAdjust className="w-100 flex relative" dense>
+              {children}
+            </TopAppBarFixedAdjust>
+          </>
+        )}
+      />
+    </div>
   )
-
-  if (isMobile) {
-    wrapper = (
-      <>
-        <Drawer open={drawerOpen} onClose={handleDrawerClose} modal>
-          <DrawerContent>{drawerItems}</DrawerContent>
-        </Drawer>
-        <DrawerAppContent className="w-100">{wrapper}</DrawerAppContent>
-      </>
-    )
-  }
-
-  return <div className="vh-100 flex">{wrapper}</div>
 }
 
-export default withRouter(
-  graphql<RouteComponentProps, Data>(loadingQuery)(Shell)
-)
+export default graphql<{}, Data>(loadingQuery)(Shell)
