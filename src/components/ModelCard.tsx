1cae4515882a77f626a60f19e889c1203117d415
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-03-30 16:54:59 -0300

    Add empty model page

--- src/components/ModelCard.tsx
+++ src/components/ModelCard.tsx
@@ -1,6 +1,7 @@
 import Card, { CardPrimaryContent } from '@material/react-card'
 import { Headline6 } from '@material/react-typography'
 import React from 'react'
+import { withRouter, RouteComponentProps } from 'react-router'
 
 import { IModel } from '../types/Model'
 
@@ -8,17 +9,19 @@ interface Props {
   className?: string
 }
 
-const ModelCard: React.FunctionComponent<Props & IModel> = ({
+const ModelCard: React.FunctionComponent<Props & IModel & RouteComponentProps> = ({
   className = '',
+  id,
   name,
+  history,
 }) => {
   return (
     <Card outlined className={className}>
-      <CardPrimaryContent className="pa2">
+      <CardPrimaryContent className="pa2" onClick={() => history.push(`/m/${id}`)}>
         <Headline6>{name}</Headline6>
       </CardPrimaryContent>
     </Card>
   )
 }
 
-export default ModelCard
+export default withRouter(ModelCard)
