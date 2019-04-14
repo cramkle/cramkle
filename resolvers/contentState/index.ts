57539e9cf8ac5fed026966c1be4c27d89094203c
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-04-14 15:06:55 -0300

    Add default value to entityMap

--- resolvers/contentState/index.ts
+++ resolvers/contentState/index.ts
@@ -5,6 +5,7 @@ import { ContentState } from '../../models'
 export const root: IResolvers = {
   ContentState: {
     id: root => root._id.toString(),
+    entityMap: root => root.entityMap || {},
   },
 }
 
