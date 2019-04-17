f42f9c34c7313d5aaa3024f2102ec9890b94f7a1
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-04-17 20:05:11 -0300

    Add module definition for css files

--- src/typings/styles.d.ts
+++ src/typings/styles.d.ts
@@ -7,3 +7,13 @@ declare module '*.scss' {
 
   export = styleObj
 }
+
+declare module '*.css' {
+  interface Module {
+    [className: string]: string
+  }
+
+  const styleObj: Module
+
+  export = styleObj
+}
