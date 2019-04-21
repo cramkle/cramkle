458da582be23f6935020fe3d755700a025f2449d
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-04-20 22:53:03 -0300

    Add title on model page

--- src/components/pages/ModelPage.tsx
+++ src/components/pages/ModelPage.tsx
@@ -4,6 +4,7 @@ import TabBar from '@material/react-tab-bar'
 import { Headline4, Caption, Body1, Body2 } from '@material/react-typography'
 import React, { useEffect, useState } from 'react'
 import { compose, graphql, ChildProps } from 'react-apollo'
+import { Helmet } from 'react-helmet'
 import { RouteComponentProps } from 'react-router'
 
 import DeleteModelButton from '../DeleteModelButton'
@@ -57,59 +58,64 @@ const ModelPage: React.FunctionComponent<ChildProps<Props, Data>> = ({
   }
 
   return (
-    <div className="pa3 ph4-m ph6-l">
-      <BackButton />
-      <div className="flex justify-between">
-        <Headline4>{cardModel.name}</Headline4>
+    <>
+      <Helmet title={cardModel.name} />
+      <div className="pa3 ph4-m ph6-l">
+        <BackButton />
+        <div className="flex justify-between">
+          <Headline4>{cardModel.name}</Headline4>
 
-        <DeleteModelButton model={cardModel} />
-      </div>
+          <DeleteModelButton model={cardModel} />
+        </div>
 
-      <Body1 className="dib mv3">Templates</Body1>
-      {cardModel.templates.length ? (
-        <>
-          <TabBar
-            activeIndex={selectedTemplate}
-            handleActiveIndexUpdate={handleTemplateSelect}
-          >
-            {cardModel.templates.map(template => (
-              <Tab key={template.id}>{template.name}</Tab>
+        <Body1 className="dib mv3">Templates</Body1>
+        {cardModel.templates.length ? (
+          <>
+            <TabBar
+              activeIndex={selectedTemplate}
+              handleActiveIndexUpdate={handleTemplateSelect}
+            >
+              {cardModel.templates.map(template => (
+                <Tab key={template.id}>{template.name}</Tab>
+              ))}
+            </TabBar>
+
+            {cardModel.templates.map((template, index) => (
+              <div hidden={selectedTemplate !== index} key={template.id}>
+                <Caption className="dib mt3">Template front side</Caption>
+                <TemplateEditor
+                  initialContentState={template.frontSide}
+                  fields={cardModel.fields}
+                />
+
+                <Caption className="dib mt3">Template back side</Caption>
+                <TemplateEditor
+                  initialContentState={template.backSide}
+                  fields={cardModel.fields}
+                />
+              </div>
             ))}
-          </TabBar>
+          </>
+        ) : (
+          <Body2>
+            You haven&apos;t created any templates on this model yet.
+          </Body2>
+        )}
 
-          {cardModel.templates.map((template, index) => (
-            <div hidden={selectedTemplate !== index} key={template.id}>
-              <Caption className="dib mt3">Template front side</Caption>
-              <TemplateEditor
-                initialContentState={template.frontSide}
-                fields={cardModel.fields}
-              />
-
-              <Caption className="dib mt3">Template back side</Caption>
-              <TemplateEditor
-                initialContentState={template.backSide}
-                fields={cardModel.fields}
-              />
-            </div>
-          ))}
-        </>
-      ) : (
-        <Body2>You haven&apos;t created any templates on this model yet.</Body2>
-      )}
-
-      <Body1 className="mv3">Fields</Body1>
-      {cardModel.fields.length ? (
-        <List dense>
-          {cardModel.fields.map(field => (
-            <ListItem key={field.id}>
-              <ListItemText primaryText={field.name} />
-            </ListItem>
-          ))}
-        </List>
-      ) : (
-        <Body2 className="mv3">This model doesn&apos;t have any fields</Body2>
-      )}
-    </div>
+        <Body1 className="mv3">Fields</Body1>
+        {cardModel.fields.length ? (
+          <List dense>
+            {cardModel.fields.map(field => (
+              <ListItem key={field.id}>
+                <ListItemText primaryText={field.name} />
+              </ListItem>
+            ))}
+          </List>
+        ) : (
+          <Body2 className="mv3">This model doesn&apos;t have any fields</Body2>
+        )}
+      </div>
+    </>
   )
 }
 
