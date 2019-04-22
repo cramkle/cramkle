6f5820180914f72848eade0c123a72a1a02afa81
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-04-21 23:33:23 -0300

    Fix mentions popup positioning

--- src/components/editor/MentionsPopup.tsx
+++ src/components/editor/MentionsPopup.tsx
@@ -1,3 +1,4 @@
+import Card from '@material/react-card'
 import List, { ListItem, ListItemText } from '@material/react-list'
 import { SelectionState } from 'draft-js'
 import React from 'react'
@@ -28,6 +29,62 @@ interface Props {
   characterOffset: number
 }
 
+const findRelativeParentElement = (
+  element: HTMLElement
+): HTMLElement | null => {
+  if (!element) {
+    return null
+  }
+
+  const position = window.getComputedStyle(element).getPropertyValue('position')
+  if (position !== 'static') {
+    return element
+  }
+
+  return findRelativeParentElement(element.parentElement)
+}
+
+const getStyleForSelectionRect = (
+  selectionRect: DOMRect,
+  offset: number
+): object => {
+  const parent = findRelativeParentElement(
+    document.getElementById('portal-anchor')
+  )
+
+  const relativeRect = {
+    scrollLeft: 0,
+    scrollTop: 0,
+    left: 0,
+    top: 0,
+  }
+
+  if (parent) {
+    relativeRect.scrollLeft = parent.scrollLeft
+    relativeRect.scrollTop = parent.scrollTop
+
+    const relativeParentRect = parent.getBoundingClientRect()
+    relativeRect.left = selectionRect.left - relativeParentRect.left
+    relativeRect.top = selectionRect.bottom - relativeParentRect.top
+  } else {
+    relativeRect.scrollTop =
+      window.pageYOffset || document.documentElement.scrollTop
+    relativeRect.scrollLeft =
+      window.pageXOffset || document.documentElement.scrollLeft
+
+    relativeRect.top = selectionRect.bottom
+    relativeRect.left = selectionRect.left
+  }
+
+  const left = relativeRect.left + relativeRect.scrollLeft + offset
+  const top = relativeRect.top + relativeRect.scrollTop + offset
+
+  return {
+    left,
+    top,
+  }
+}
+
 const MentionsPopup: React.FunctionComponent<Props> = ({
   onMentionSelect,
   mentionableEntries,
@@ -51,26 +108,23 @@ const MentionsPopup: React.FunctionComponent<Props> = ({
     highlightedMentionable
   )
 
+  const style = getStyleForSelectionRect(selectionRect, offset)
+
   return (
     <Portal>
-      <div
-        style={{
-          top: selectionRect.y + offset,
-          left: selectionRect.x + offset,
-        }}
-        className="pv2 ba b--near-black z-2 absolute bg-white"
-      >
+      <Card style={style} className="pv2 z-2 absolute">
         <List selectedIndex={highlightedMentionableIndex} dense>
           {mentionableEntries.map(mentionable => (
             <ListItem
               key={mentionable.id}
+              tabIndex={0}
               onClick={evt => onMentionSelect(mentionable, evt)}
             >
               <ListItemText primaryText={mentionable.name} />
             </ListItem>
           ))}
         </List>
-      </div>
+      </Card>
     </Portal>
   )
 }
