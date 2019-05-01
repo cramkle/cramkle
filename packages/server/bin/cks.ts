95fab9b1674b4e28096e7cf737feb804797bcb51
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-05-01 15:25:35 -0300

    Add implementation for cli entrypoint

--- packages/server/bin/cks.ts
+++ packages/server/bin/cks.ts
@@ -1,3 +1,23 @@
 #!/usr/bin/env node
 
-console.log('hello')
+const commands: { [command: string]: () => Promise<void> } = {
+  build: async () => await import('../scripts/build').then(i => i.default()),
+  start: async () => await import('../src/start').then(i => i.default()),
+  dev: async () => await import('../src/start-dev').then(i => i.default()),
+}
+
+const command = process.argv[2]
+
+const hasCommand = Boolean(commands[command])
+
+if (!hasCommand) {
+  console.error(
+    `Unkown command ${command}. Available ones are 'dev', 'build' and 'start'.`
+  )
+  process.exit(0)
+}
+
+const defaultEnv = command === 'dev' ? 'development' : 'production'
+process.env.NODE_ENV = process.env.NODE_ENV || defaultEnv
+
+commands[command]()
