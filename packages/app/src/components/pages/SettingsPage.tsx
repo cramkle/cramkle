4f394db4ad9630081249f0fc2d13e86646c75e8a
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-05-05 23:09:42 -0300

    Add general settings with language selector

--- packages/app/src/components/pages/SettingsPage.tsx
+++ packages/app/src/components/pages/SettingsPage.tsx
@@ -1,7 +1,19 @@
+import { Trans } from '@lingui/macro'
+import { Headline5 } from '@material/react-typography'
 import React from 'react'
 
+import GeneralSettings from '../GeneralSettings'
+import Container from '../views/Container'
+
 const SettingsPage: React.FunctionComponent = () => {
-  return <div>this is not implemented yet</div>
+  return (
+    <Container>
+      <Headline5>
+        <Trans>Settings</Trans>
+      </Headline5>
+      <GeneralSettings />
+    </Container>
+  )
 }
 
 export default SettingsPage
