7195bab0fdb34f8213725974738df5febed50b71
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-05-11 22:50:34 -0300

    Update mention span background color to secondary

--- packages/app/src/components/editor/MentionSpan.tsx
+++ packages/app/src/components/editor/MentionSpan.tsx
@@ -1,9 +1,10 @@
+import cx from 'classnames'
 import React from 'react'
 
 import styles from './MentionSpan.css'
 
 const MentionSpan: React.FunctionComponent = ({ children }) => {
-  return <span className={styles.mention}>{children}</span>
+  return <span className={cx(styles.mention, 'relative')}>{children}</span>
 }
 
 export default MentionSpan
