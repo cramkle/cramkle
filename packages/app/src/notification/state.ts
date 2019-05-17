470ecb710396bb1b1ef78d785bc7d81c29350790
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-05-16 21:11:05 -0300

    Add removed notification event

--- packages/app/src/notification/state.ts
+++ packages/app/src/notification/state.ts
@@ -1,3 +1,5 @@
+import * as uuid from 'uuid'
+
 import Emitter from './event'
 
 interface NotificationAction {
@@ -13,12 +15,38 @@ interface NotificationMessage {
   stacked?: boolean
 }
 
+interface NotificationEvent {
+  id: string
+  notification: NotificationMessage
+}
+
+interface NotificationRemovedEvent {
+  id: string
+}
+
 export default class NotificationState {
-  private notificationEmitter = new Emitter<NotificationMessage>()
+  private notificationEmitter = new Emitter<NotificationEvent>()
+  private removedNotificationEmitter = new Emitter<NotificationRemovedEvent>()
+
+  private notifications = new Map<string, NotificationMessage>()
 
   public onNotification = this.notificationEmitter.event
+  public onNotificationRemoved = this.removedNotificationEmitter.event
 
   public addNotification(notification: NotificationMessage) {
-    this.notificationEmitter.emit(notification)
+    const id = uuid.v4()
+    this.notifications.set(id, notification)
+
+    this.notificationEmitter.emit({ id, notification })
+
+    return id
+  }
+
+  public removeNotification(id: string) {
+    if (this.notifications.has(id)) {
+      this.notifications.delete(id)
+
+      this.removedNotificationEmitter.emit({ id })
+    }
   }
 }
