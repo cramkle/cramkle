4575ddb1e584a0acb2bcc002caf3f89ab10c360f
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-05-19 15:42:03 -0300

    Add translation for not found page

--- packages/app/src/components/pages/NotFoundPage.tsx
+++ packages/app/src/components/pages/NotFoundPage.tsx
@@ -1,7 +1,12 @@
+import { Trans } from '@lingui/macro'
 import React from 'react'
 
 const NotFoundPage: React.FunctionComponent = () => {
-  return <div>The page you requested wasn&apos;t found</div>
+  return (
+    <div>
+      <Trans>The page you requested wasn't found</Trans>
+    </div>
+  )
 }
 
 export default NotFoundPage
