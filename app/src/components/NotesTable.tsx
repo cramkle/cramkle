1b57fc690bde5779b8848a76f099bc3a05ca728c
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-05-25 20:57:42 -0300

    Add notes table and add note page with placeholders

---
+++ app/src/components/NotesTable.tsx
@@ -0,0 +1,27 @@
+import { Trans } from '@lingui/macro'
+import React from 'react'
+
+import Card from 'views/Card'
+import { Body2 } from 'views/Typography'
+
+import { DeckQuery_deck_notes } from 'pages/__generated__/DeckQuery'
+
+interface Props {
+  notes: DeckQuery_deck_notes[]
+}
+
+const NotesTable: React.FC<Props> = ({ notes }) => {
+  if (!notes.length) {
+    return (
+      <Card className="w-100 mt2 pv3 ph2 flex flex-row justify-center" outlined>
+        <Body2>
+          <Trans>You haven't created any notes on this deck yet!</Trans>
+        </Body2>
+      </Card>
+    )
+  }
+
+  return <span>hello</span>
+}
+
+export default NotesTable
--- app/src/components/pages/__generated__/DeckQuery.ts
+++ app/src/components/pages/__generated__/DeckQuery.ts
@@ -6,6 +6,70 @@
 // GraphQL query operation: DeckQuery
 // ====================================================
 
+export interface DeckQuery_deck_notes_values_data_blocks_inlineStyleRanges {
+  __typename: "InlineStyleRange";
+  style: string | null;
+  offset: number | null;
+  length: number | null;
+}
+
+export interface DeckQuery_deck_notes_values_data_blocks_entityRanges {
+  __typename: "EntityRange";
+  key: number;
+  length: number;
+  offset: number;
+}
+
+export interface DeckQuery_deck_notes_values_data_blocks {
+  __typename: "Block";
+  key: string;
+  type: string;
+  text: string;
+  depth: number;
+  inlineStyleRanges: (DeckQuery_deck_notes_values_data_blocks_inlineStyleRanges | null)[] | null;
+  entityRanges: (DeckQuery_deck_notes_values_data_blocks_entityRanges | null)[] | null;
+  data: any | null;
+}
+
+export interface DeckQuery_deck_notes_values_data {
+  __typename: "ContentState";
+  id: string;
+  blocks: (DeckQuery_deck_notes_values_data_blocks | null)[] | null;
+  entityMap: any | null;
+}
+
+export interface DeckQuery_deck_notes_values {
+  __typename: "FieldValue";
+  /**
+   * Field data
+   */
+  data: DeckQuery_deck_notes_values_data | null;
+}
+
+export interface DeckQuery_deck_notes_model {
+  __typename: "CardModel";
+  /**
+   * Name of this card model (e.g. "Basic", "Basic with Reversed")
+   */
+  name: string | null;
+}
+
+export interface DeckQuery_deck_notes {
+  __typename: "Note";
+  /**
+   * Note id
+   */
+  id: string;
+  /**
+   * Values of this note
+   */
+  values: (DeckQuery_deck_notes_values | null)[] | null;
+  /**
+   * Model of this note
+   */
+  model: DeckQuery_deck_notes_model | null;
+}
+
 export interface DeckQuery_deck {
   __typename: "Deck";
   /**
@@ -24,6 +88,10 @@ export interface DeckQuery_deck {
    * Description of the deck
    */
   description: string | null;
+  /**
+   * Notes contained in this deck
+   */
+  notes: (DeckQuery_deck_notes | null)[] | null;
 }
 
 export interface DeckQuery {
