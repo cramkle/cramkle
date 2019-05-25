d568e639fd33de18e979310e9bab449612bf7802
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-05-25 20:54:06 -0300

    Fix not found page layout on mobile

--- packages/app/src/components/pages/NotFoundPage.tsx
+++ packages/app/src/components/pages/NotFoundPage.tsx
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom'
 
 const NotFoundPage: React.FunctionComponent = () => {
   return (
-    <div className="flex flex-column items-center justify-center min-vh-100">
-      <Headline5 className="b">
+    <div className="flex flex-column items-center justify-center ph3 ph0-ns min-vh-100">
+      <Headline5 className="b tc tl-ns">
         <Trans>We couldn't find the page you requested</Trans>
       </Headline5>
-      <Body1 className="mt1">
+      <Body1 className="mt2 mt1-ns tc tl-ns">
         <Trans>This link may be broken or the typed URL is incorrect.</Trans>
       </Body1>
-      <Link className="mt3" to="/">
+      <Link className="mt4 mt3-ns" to="/">
         <Trans>Go to Home</Trans>
       </Link>
     </div>
