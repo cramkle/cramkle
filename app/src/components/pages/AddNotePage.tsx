580c9569eb121d1ce25756a46b09ac24ce6030df
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-05-25 22:52:03 -0300

    Add model selector and field value editor

--- app/src/components/pages/AddNotePage.tsx
+++ app/src/components/pages/AddNotePage.tsx
@@ -1,7 +1,120 @@
-import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import { Trans, t } from '@lingui/macro'
+import { useLingui } from '@lingui/react'
+import Select, { Option } from '@material/react-select'
+import gql from 'graphql-tag'
+import React, { useState } from 'react'
 
-const AddNotePage = () => {
-  return <span>hello, you're here to add a note, right?</span>
+import BackButton from 'components/BackButton'
+import FieldValueEditor from 'components/FieldValueEditor'
+import Button from 'views/Button'
+import Container from 'views/Container'
+import { Body2, Caption, Headline5, Subtitle1 } from 'views/Typography'
+import useTopBarLoading from 'hooks/useTopBarLoading'
+import { ModelsQuery } from './__generated__/ModelsQuery'
+
+const MODELS_QUERY = gql`
+  query Notes_ModelsQuery {
+    cardModels {
+      id
+      name
+      fields {
+        id
+        name
+      }
+    }
+  }
+`
+
+const AddNotePage: React.FC = () => {
+  const {
+    data: { cardModels: models },
+    loading,
+  } = useQuery<ModelsQuery>(MODELS_QUERY)
+
+  const { i18n } = useLingui()
+  const [selectedModelId, setSelectedModelId] = useState('')
+
+  useTopBarLoading(loading)
+
+  if (loading) {
+    return null
+  }
+
+  const selectedModel = models.find(model => model.id === selectedModelId)
+
+  if (!models.length) {
+    return (
+      <Container>
+        <Headline5>
+          <Trans>You haven't created any models yet.</Trans>
+        </Headline5>
+      </Container>
+    )
+  }
+
+  return (
+    <Container>
+      <BackButton />
+
+      <Headline5>
+        <Trans>Create Note</Trans>
+      </Headline5>
+
+      <div className="flex flex-column mt3">
+        {
+          // @ts-ignore
+          <Select
+            label={i18n._(t`Select a model`)}
+            value={selectedModelId}
+            onChange={e =>
+              setSelectedModelId((e.target as HTMLSelectElement).value)
+            }
+          >
+            <Option value="" disabled />
+            {models.map(model => (
+              <Option key={model.id} value={model.id}>
+                {model.name}
+              </Option>
+            ))}
+          </Select>
+        }
+
+        {selectedModelId !== '' && (
+          <>
+            <Subtitle1 className="mt2">
+              <Trans>Fields</Trans>
+            </Subtitle1>
+
+            {selectedModel.fields.length > 0 ? (
+              <>
+                {selectedModel.fields.map(field => (
+                  <React.Fragment key={field.id}>
+                    <Caption className="mt2" key={field.id}>
+                      {field.name}
+                    </Caption>
+
+                    <FieldValueEditor className="mt1" />
+                  </React.Fragment>
+                ))}
+
+                <Button raised className="mt3 self-start">
+                  <Trans>Add Note</Trans>
+                </Button>
+              </>
+            ) : (
+              <Body2 className="mt2">
+                <Trans>
+                  The selected model doesn't have any fields. Please, select
+                  another one
+                </Trans>
+              </Body2>
+            )}
+          </>
+        )}
+      </div>
+    </Container>
+  )
 }
 
 export default AddNotePage
