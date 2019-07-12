bacde31a25d7ae6cf214f14b7b6f511d93b9a682
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-07-11 21:57:00 -0300

    upgrade draft-js package

--- packages/app/src/components/editor/MentionsEditor.tsx
+++ packages/app/src/components/editor/MentionsEditor.tsx
@@ -1,4 +1,10 @@
-import { Editor, EditorProps, EditorState, DraftHandleValue } from 'draft-js'
+import {
+  Editor,
+  EditorProps,
+  EditorState,
+  DraftHandleValue,
+  getDefaultKeyBinding,
+} from 'draft-js'
 import React, { useEffect, useReducer, useCallback, useRef } from 'react'
 
 import MentionsPopup from './MentionsPopup'
@@ -6,7 +12,7 @@ import searchMentions from './searchMentions'
 import replaceMentionInEditorState from './replaceMentionInEditorState'
 import { MentionableEntry } from '../../model/MentionableEntry'
 
-interface Props extends EditorProps {
+interface Props extends Omit<EditorProps, 'keyBindingFn' | 'handleKeyCommand'> {
   mentionSource: MentionableEntry[]
   autoHighlight?: boolean
   autoUpdateHighlight?: boolean
@@ -52,10 +58,6 @@ const MentionsEditor: React.FunctionComponent<Props> = ({
   autoHighlight = true,
   autoUpdateHighlight = true,
   onChange,
-  onUpArrow,
-  onDownArrow,
-  onTab,
-  onEscape,
   onBlur,
   handleReturn: handleContentReturn,
   ariaAutoComplete = 'list',
@@ -113,59 +115,46 @@ const MentionsEditor: React.FunctionComponent<Props> = ({
     searchMentions(mentionSource, selection, contentState, onShowMentions)
   }, [editorState, mentionSource, onShowMentions])
 
-  const handleEscape = (evt: React.KeyboardEvent) => {
-    onEscape && onEscape(evt)
-
-    if (mentionableEntries.length) {
-      evt.stopPropagation()
-      dispatch({ type: 'reset' })
-    }
-  }
-
   const handleMentionHighlight = useCallback((mention: MentionableEntry) => {
     dispatch({ type: 'update_highlighted', highlightedMentionable: mention })
   }, [])
 
-  const handleUpArrow = (evt: React.KeyboardEvent) => {
-    if (mentionableEntries.length) {
-      evt.preventDefault()
-
-      const length = mentionableEntries.length
-      const selectedIndex = mentionableEntries.indexOf(highlightedMentionable)
-
-      let highlighted = null
-
-      if (!highlightedMentionable) {
-        highlighted = mentionableEntries[length - 1]
-      } else if (selectedIndex > 0) {
-        highlighted = mentionableEntries[selectedIndex - 1]
-      }
-
-      handleMentionHighlight(highlighted)
-    } else {
-      onUpArrow && onUpArrow(evt)
+  const moveSelectionUp = () => {
+    if (!mentionableEntries.length) {
+      return
     }
+
+    const length = mentionableEntries.length
+    const selectedIndex = mentionableEntries.indexOf(highlightedMentionable)
+
+    let highlighted = null
+
+    if (!highlightedMentionable) {
+      highlighted = mentionableEntries[length - 1]
+    } else if (selectedIndex > 0) {
+      highlighted = mentionableEntries[selectedIndex - 1]
+    }
+
+    handleMentionHighlight(highlighted)
   }
 
-  const handleDownArrow = (evt: React.KeyboardEvent) => {
-    if (mentionableEntries.length) {
-      evt.preventDefault()
-
-      const length = mentionableEntries.length
-      const selectedIndex = mentionableEntries.indexOf(highlightedMentionable)
-
-      let highlighted = null
-
-      if (!highlightedMentionable) {
-        highlighted = mentionableEntries[0]
-      } else if (selectedIndex < length - 1) {
-        highlighted = mentionableEntries[selectedIndex + 1]
-      }
-
-      handleMentionHighlight(highlighted)
-    } else {
-      onDownArrow && onDownArrow(evt)
+  const moveSelectionDown = () => {
+    if (!mentionableEntries.length) {
+      return
     }
+
+    const length = mentionableEntries.length
+    const selectedIndex = mentionableEntries.indexOf(highlightedMentionable)
+
+    let highlighted = null
+
+    if (!highlightedMentionable) {
+      highlighted = mentionableEntries[0]
+    } else if (selectedIndex < length - 1) {
+      highlighted = mentionableEntries[selectedIndex + 1]
+    }
+
+    handleMentionHighlight(highlighted)
   }
 
   const handleBlur = (evt: React.FocusEvent) => {
@@ -187,15 +176,6 @@ const MentionsEditor: React.FunctionComponent<Props> = ({
     [characterOffset, editorState, onChange]
   )
 
-  const handleTab = (evt: React.KeyboardEvent) => {
-    if (highlightedMentionable) {
-      evt.preventDefault()
-      handleMentionSelect(highlightedMentionable)
-    } else {
-      onTab && onTab(evt)
-    }
-  }
-
   const handleReturn = (
     evt: React.KeyboardEvent,
     editorState: EditorState
@@ -207,6 +187,45 @@ const MentionsEditor: React.FunctionComponent<Props> = ({
       return handleContentReturn(evt, editorState)
     }
     return 'not-handled'
+  }
+
+  const keyBinder = (e: React.KeyboardEvent) => {
+    switch (e.keyCode) {
+      case 9: // tab
+        return 'handle-autocomplete'
+      case 27: // escape
+        return 'cancel-autocomplete'
+      case 38: // arrow up
+        return 'move-selection-up'
+      case 40: // arrow down
+        return 'move-selection-down'
+      default:
+        return getDefaultKeyBinding(e)
+    }
+  }
+
+  const handleKeyCommand = (command: string): DraftHandleValue => {
+    switch (command) {
+      case 'handle-autocomplete':
+        if (highlightedMentionable) {
+          handleMentionSelect(highlightedMentionable)
+        }
+
+        return 'handled'
+      case 'cancel-autocomplete':
+        if (mentionableEntries.length) {
+          dispatch({ type: 'reset' })
+        }
+        return 'handled'
+      case 'move-selection-up':
+        moveSelectionUp()
+        return 'handled'
+      case 'move-selection-down':
+        moveSelectionDown()
+        return 'handled'
+      default:
+        return 'not-handled'
+    }
   }
 
   const showingMentions = !!(mentionableEntries && mentionableEntries.length)
@@ -221,12 +240,10 @@ const MentionsEditor: React.FunctionComponent<Props> = ({
         spellCheck
         editorState={editorState}
         onChange={onChange}
-        onUpArrow={handleUpArrow}
-        onDownArrow={handleDownArrow}
-        onTab={handleTab}
-        onEscape={handleEscape}
         onBlur={handleBlur}
         handleReturn={handleReturn}
+        handleKeyCommand={handleKeyCommand}
+        keyBindingFn={keyBinder}
       />
       <MentionsPopup
         mentionableEntries={mentionableEntries}
