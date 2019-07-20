c8fe772d2205860a5be9269f8524451858155ff1
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-07-20 12:04:09 -0300

    use log for prod start script

--- packages/server/src/start.ts
+++ packages/server/src/start.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 
 import server from './index'
+import * as Log from './output/log'
 
 const start = () => {
   const app = express()
@@ -8,7 +9,7 @@ const start = () => {
   app.use(server)
 
   app.listen(3000, () => {
-    console.log('Server listening on port 3000')
+    Log.info('Server listening on port 3000')
   })
 }
 
