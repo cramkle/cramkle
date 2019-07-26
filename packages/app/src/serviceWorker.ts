0ccfc650d22d21ae179b5b55be3e53037c0e3f5d
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-07-26 01:42:40 -0300

    replace deprecated methods with appropriate constructors

--- packages/app/src/serviceWorker.ts
+++ packages/app/src/serviceWorker.ts
@@ -2,10 +2,10 @@
 
 import { precacheAndRoute } from 'workbox-precaching'
 import {
-  networkFirst,
-  staleWhileRevalidate,
-  cacheFirst,
-  networkOnly,
+  NetworkFirst,
+  StaleWhileRevalidate,
+  CacheFirst,
+  NetworkOnly,
 } from 'workbox-strategies'
 import { registerRoute } from 'workbox-routing'
 
@@ -20,7 +20,7 @@ declare global {
 
 precacheAndRoute(self.__WB_MANIFEST, {})
 
-const networkFirstHandler = networkFirst({}).handle
+const networkFirstHandler = new NetworkFirst().handle
 
 // api
 registerRoute(/_c/, networkFirstHandler)
@@ -28,14 +28,14 @@ registerRoute(/_c/, networkFirstHandler)
 // google fonts
 registerRoute(
   /^https:\/\/fonts\.googleapis\.com/,
-  staleWhileRevalidate({
+  new StaleWhileRevalidate({
     cacheName: 'google-fonts-stylesheets',
   }).handle
 )
 
 registerRoute(
   /^https:\/\/fonts\.gstatic\.com/,
-  cacheFirst({
+  new CacheFirst({
     cacheName: 'google-fonts-webfonts',
     cacheableResponse: {
       statuses: [0, 200],
@@ -51,7 +51,7 @@ registerRoute(
 // image assets
 registerRoute(
   IMAGE_REGEX,
-  cacheFirst({
+  new CacheFirst({
     cacheName: 'images',
     expiration: {
       maxEntries: 60,
@@ -64,15 +64,15 @@ registerRoute(
 // javascript and css
 registerRoute(
   JS_CSS_REGEX,
-  (process.env.NODE_ENV === 'development' ? networkOnly : staleWhileRevalidate)(
-    {
-      cacheName: 'static-resources',
-    }
-  ).handle
+  new (process.env.NODE_ENV === 'development'
+    ? NetworkOnly
+    : StaleWhileRevalidate)({
+    cacheName: 'static-resources',
+  }).handle
 )
 
 if (process.env.NODE_ENV === 'development') {
-  registerRoute(/(__webpack_hmr|hot-update)/, networkOnly({}).handle)
+  registerRoute(/(__webpack_hmr|hot-update)/, new NetworkOnly().handle)
 } else {
   registerRoute(/^https:\/\/(www\.)?cramkle\.com/, networkFirstHandler)
 }
