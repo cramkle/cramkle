6b5a027f7e05cd038cebe7dacd52da52648bd5f9
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-08-25 16:05:42 -0300

    add ripple to style button

--- packages/app/src/components/editor/StyleButton.tsx
+++ packages/app/src/components/editor/StyleButton.tsx
@@ -1,13 +1,14 @@
 import { MessageDescriptor } from '@lingui/core'
 import { useLingui } from '@lingui/react'
 import { useControlledTabIndex } from '@lucasecdb/react-tab-controller'
-import cx from 'classnames'
+import classNames from 'classnames'
 import React, { memo, useRef } from 'react'
 
 import Icon from 'views/Icon'
 import { IconTypes } from 'views/IconTypes'
+import { useRipple } from 'views/Ripple'
 
-import styles from './StyleButton.css'
+import styles from './StyleButton.scss'
 
 export interface Style {
   label: MessageDescriptor | string
@@ -30,16 +31,22 @@ const StyleButton: React.FunctionComponent<Props> = ({
   const { i18n } = useLingui()
   const buttonRef = useRef<HTMLButtonElement>(null)
 
+  const { rippleStyle, rippleClasses } = useRipple({
+    surfaceRef: buttonRef,
+    unbounded: true,
+  })
+
   const handleToggle = (e: React.MouseEvent<HTMLSpanElement>) => {
     e.preventDefault()
     onToggle(style)
   }
 
-  const className = cx(
+  const className = classNames(
     styles.button,
-    'mdc-typography relative pointer mr2 pv1 dib bn bg-surface flex items-center',
+    rippleClasses,
+    'mdc-typography mdc-ripple-surface relative pointer mr2 pv1 dib bn br1 flex items-center',
     {
-      'c-primary': active,
+      'c-primary mdc-ripple-surface--primary': active,
       [styles.buttonSelected]: active,
     }
   )
@@ -50,6 +57,7 @@ const StyleButton: React.FunctionComponent<Props> = ({
 
   return (
     <button
+      style={rippleStyle}
       className={className}
       onClick={handleToggle}
       ref={buttonRef}
