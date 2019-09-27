f5be9841f18672ec4e82541548fdb3e052103fb2
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-09-26 21:36:01 -0300

    remove server and use react-app-server package

--- packages/server/bin/cks-bin.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-// eslint-disable-next-line
-const path = require('path')
-require(path.join(__dirname, '../dist/bin/cks'))
--- packages/server/config/webpack/types.ts
+++
@@ -1,4 +0,0 @@
-export interface Options {
-  isServer?: boolean
-  dev?: boolean
-}
