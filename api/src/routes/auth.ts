26df6b418d70cf13b7937f0596cf60410bacc0b3
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-09-26 22:12:22 -0300

    fix some typings errors in the api

--- api/src/routes/auth.ts
+++ api/src/routes/auth.ts
@@ -4,7 +4,7 @@ import { authenticate } from 'passport'
 const router = Router()
 
 router.post('/login', authenticate('local'), (req, res) => {
-  res.json({ id: req.user._id, username: req.user.username })
+  res.json({ id: req.user!._id, username: req.user!.username })
 })
 
 router.post('/logout', (req, res) => {
--- api/src/typings/global.d.ts
+++ api/src/typings/global.d.ts
@@ -4,4 +4,11 @@ declare global {
   interface Context {
     user: User
   }
+
+  namespace Express {
+    interface User extends UserDocument {
+      _id: string
+      username: string
+    }
+  }
 }
