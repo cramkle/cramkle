eddfa06f7af7d28e1bf08840cdc4717137c268dd
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-04-12 15:45:24 -0300

    add basic notes table implementation

--- app/__generated__/globalTypes.ts
+++ app/__generated__/globalTypes.ts
@@ -7,6 +7,12 @@
 // START Enums and Input Objects
 //==============================================================
 
+export enum CardStatus {
+  DUE = "DUE",
+  LEARNING = "LEARNING",
+  NEW = "NEW",
+}
+
 export interface BlockInput {
   key: string;
   type: string;
