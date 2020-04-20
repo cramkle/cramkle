ad995890d96f62899c7ce0af7bf8e3a1e190650d
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-04-20 08:39:56 -0300

    fix lint issues

--- app/scripts/update-changelog.js
+++ app/scripts/update-changelog.js
@@ -28,10 +28,7 @@ const update = async () => {
 
   const year = now.getFullYear()
   const month = (now.getUTCMonth() + 1).toString().padStart(2, '0')
-  const day = now
-    .getUTCDate()
-    .toString()
-    .padStart(2, '0')
+  const day = now.getUTCDate().toString().padStart(2, '0')
 
   const newChangelog = `
 ${header}
