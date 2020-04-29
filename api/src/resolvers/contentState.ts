ef796d01af1205272beed67f0671a759351efabd
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-04-29 13:56:17 -0300

    fix: Template interpolation in flashCard renderer not working (#140)

--- api/src/resolvers/contentState.ts
+++ api/src/resolvers/contentState.ts
@@ -1,11 +1,39 @@
 import { IResolvers } from 'graphql-tools'
 
 import { ContentStateDocument } from '../mongo/ContentState'
-import { globalIdField } from '../utils/graphqlID'
+import { encodeGlobalId, globalIdField } from '../utils/graphqlID'
 
 export const root: IResolvers = {
   ContentState: {
     id: globalIdField(),
-    entityMap: (root: ContentStateDocument) => root.entityMap || {},
+    entityMap: (root: ContentStateDocument) => {
+      const entityMap = root.entityMap || {}
+
+      const updatedEntityMap = Object.entries(entityMap)
+        .map(([key, value]) => {
+          if (value?.type !== 'TAG') {
+            return [key, value] as const
+          }
+
+          const encodedFieldId = encodeGlobalId('Field', (value.data as any).id)
+
+          const data = {
+            ...value.data,
+            id: encodedFieldId,
+            name: {
+              ...(value.data as any).name,
+              id: encodedFieldId,
+            },
+          }
+
+          return [key, { ...value, data }] as const
+        })
+        .reduce(
+          (obj, [key, value]) => Object.assign({}, obj, { [key]: value }),
+          {}
+        )
+
+      return updatedEntityMap
+    },
   },
 }
