4da091e69f7e2425388362465fdb667a416e7993
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-05-01 23:43:53 -0300

    feat: Add mutations to reset the user's password

--- api/src/resolvers/user.ts
+++ api/src/resolvers/user.ts
@@ -1,7 +1,13 @@
+import { createHmac } from 'crypto'
+
 import { AuthenticationError } from 'apollo-server'
+import { differenceInHours } from 'date-fns'
 import { IResolverObject, IResolvers } from 'graphql-tools'
+import { ObjectId } from 'mongodb'
+import { Types } from 'mongoose'
 
 import { UserModel } from '../mongo'
+import { UserDocument } from '../mongo/User'
 import { globalIdField } from '../utils/graphqlID'
 
 export const root: IResolvers = {
@@ -22,11 +28,63 @@ export const queries: IResolverObject = {
   },
 }
 
+const createResetPasswordHashValue = ({
+  userId,
+  email,
+  password,
+  lastLogin,
+  timestamp,
+}: {
+  userId: string
+  email: string
+  password: string
+  timestamp: number
+  lastLogin?: number
+}) => {
+  return (
+    userId +
+    email +
+    password +
+    timestamp.toString() +
+    (lastLogin?.toString() ?? '')
+  )
+}
+
+const createHashWithTimestamp = (timestamp: number, user: UserDocument) => {
+  const hashString = createHmac(
+    'sha256',
+    process.env.RESET_PASSWORD_TOKEN ?? '__development__'
+  )
+    .update(
+      createResetPasswordHashValue({
+        userId: user._id.toString(),
+        email: user.email,
+        password: user.password,
+        lastLogin: user.lastLogin?.getTime(),
+        timestamp,
+      })
+    )
+    .digest('hex')
+
+  return hashString
+}
+
 interface UpdateProfileInput {
   email: string
   username: string
   password: string
   confirmPassword: string
+}
+
+interface RequestPasswordResetInput {
+  email: string
+}
+
+interface ResetPasswordInput {
+  token: string
+  timestamp: string
+  userId: string
+  newPassword: string
 }
 
 export const mutations: IResolverObject = {
@@ -72,5 +130,50 @@ export const mutations: IResolverObject = {
     await user?.hashifyAndSave()
 
     return user
+  },
+  requestPasswordReset: async (
+    _,
+    { input }: { input: RequestPasswordResetInput }
+  ) => {
+    const user = await UserModel.findOne({ email: input.email })
+
+    if (!user) {
+      return { success: false }
+    }
+
+    const userId = user._id as Types.ObjectId
+
+    const todayTimestamp = Date.now()
+
+    const hashString = createHashWithTimestamp(todayTimestamp, user)
+
+    // should send email instead
+    console.log(`/reset/${userId}/${todayTimestamp.toString(36)}-${hashString}`)
+
+    return { success: true }
+  },
+  resetPassword: async (_, { input }: { input: ResetPasswordInput }) => {
+    const { timestamp, token, userId, newPassword } = input
+
+    const user = await UserModel.findById(userId)
+
+    if (!user) {
+      return { success: false }
+    }
+
+    const parsedTimestamp = parseInt(timestamp, 36)
+
+    if (token !== createHashWithTimestamp(parsedTimestamp, user)) {
+      return { success: false }
+    }
+
+    if (differenceInHours(parsedTimestamp, Date.now()) > 24) {
+      return { success: false }
+    }
+
+    user.password = newPassword
+    await user.hashifyAndSave()
+
+    return { success: true }
   },
 }
