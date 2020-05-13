2d0b77b8199a1d0d9220cc292e8af0a2c2b3eb85
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-05-12 22:49:16 -0300

    remove material drawer, list and top-app-bar

--- app/src/components/pages/LandingPage.tsx
+++ app/src/components/pages/LandingPage.tsx
@@ -5,16 +5,11 @@ import { Link } from 'react-router-dom'
 
 import bgUrl from '../../assets/landing-bg.svg'
 import { ReactComponent as Logo } from '../../assets/logo.svg'
+import AppName from '../AppName'
 import Footer from '../Footer'
 import RegisterForm from '../forms/RegisterForm'
 import Button from '../views/Button'
-import TopAppBar, {
-  TopAppBarFixedAdjust,
-  TopAppBarIcon,
-  TopAppBarRow,
-  TopAppBarSection,
-  TopAppBarTitle,
-} from '../views/TopAppBar'
+import { Header, HeaderContent, HeaderSection } from '../views/Header'
 import { Headline2, Headline4 } from '../views/Typography'
 
 const LandingPage: React.FunctionComponent = () => {
@@ -22,63 +17,59 @@ const LandingPage: React.FunctionComponent = () => {
 
   return (
     <>
-      <TopAppBar fixed>
-        <TopAppBarRow>
-          <TopAppBarSection align="start">
-            <TopAppBarIcon>
-              <Logo width={24} />
-            </TopAppBarIcon>
-            <TopAppBarTitle>Cramkle</TopAppBarTitle>
-          </TopAppBarSection>
-          <TopAppBarSection align="end">
+      <Header>
+        <HeaderContent>
+          <HeaderSection className="flex items-center pl-1">
+            <Logo width={24} />
+            <AppName className="ml-2" />
+          </HeaderSection>
+          <HeaderSection align="end">
             <Button unelevated onClick={() => history.push('/register')}>
               <Trans>Sign Up</Trans>
             </Button>
             <Button onClick={() => history.push('/login')} className="ml-2">
               <Trans>Login</Trans>
             </Button>
-          </TopAppBarSection>
-        </TopAppBarRow>
-      </TopAppBar>
-      <TopAppBarFixedAdjust>
-        <div
-          className="flex bg-primary text-on-primary min-h-screen w-full"
-          style={{ backgroundImage: `url(${bgUrl})` }}
-        >
-          <div className="flex flex-col sm:flex-row justify-around items-center w-full mx-2 lg:mx-16 py-16 sm:py-32">
-            <div
-              className="flex flex-wrap py-8 sm:py-0 w-full"
-              style={{ maxWidth: 450 }}
-            >
-              <Headline2 className="text-4xl sm:text-5xl font-bold leading-tight">
-                <Trans>Make sure your knowledge will last</Trans>
-              </Headline2>
-              <Headline4 className="text-base sm:text-2xl mt-4 leading-tight">
-                <Trans>
-                  Optimize your knowledge retention with this effective study
-                  method.
-                </Trans>
-              </Headline4>
-            </div>
-            <div
-              className="inline-flex flex-col items-center w-full"
-              style={{ maxWidth: 450 }}
-            >
-              <RegisterForm title={t`Sign up now!`} />
+          </HeaderSection>
+        </HeaderContent>
+      </Header>
+      <div
+        className="flex bg-primary text-on-primary min-h-screen w-full"
+        style={{ backgroundImage: `url(${bgUrl})` }}
+      >
+        <div className="flex flex-col sm:flex-row justify-around items-center w-full mx-2 lg:mx-16 py-16 sm:py-32">
+          <div
+            className="flex flex-wrap py-8 sm:py-0 w-full"
+            style={{ maxWidth: 450 }}
+          >
+            <Headline2 className="text-4xl sm:text-5xl font-bold leading-tight">
+              <Trans>Make sure your knowledge will last</Trans>
+            </Headline2>
+            <Headline4 className="text-base sm:text-2xl mt-4 leading-tight">
+              <Trans>
+                Optimize your knowledge retention with this effective study
+                method.
+              </Trans>
+            </Headline4>
+          </div>
+          <div
+            className="inline-flex flex-col items-center w-full"
+            style={{ maxWidth: 450 }}
+          >
+            <RegisterForm title={t`Sign up now!`} />
 
-              <span className="landing__login-text mt-4">
-                <Trans>
-                  Already have an account?{' '}
-                  <Link to="/login" className="font-bold text-on-primary">
-                    Log In
-                  </Link>
-                </Trans>
-              </span>
-            </div>
+            <span className="landing__login-text mt-4">
+              <Trans>
+                Already have an account?{' '}
+                <Link to="/login" className="font-bold text-on-primary">
+                  Log In
+                </Link>
+              </Trans>
+            </span>
           </div>
         </div>
-        <Footer />
-      </TopAppBarFixedAdjust>
+      </div>
+      <Footer />
     </>
   )
 }
