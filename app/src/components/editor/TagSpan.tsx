3deda33ec7f24770db7a1349976d1472ae8180e7
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-06-01 14:20:59 -0300

    fix: Editor not working properly with fields tags (#191)
    also adds a better description to the fields in the dialog and remove the outline in the button component

--- app/src/components/editor/TagSpan.tsx
+++ app/src/components/editor/TagSpan.tsx
@@ -1,27 +1,9 @@
-import { ContentState } from 'draft-js'
 import React from 'react'
 
-import { useTagEntriesContext } from './TagEntriesContext'
-
-interface TagSpanProps {
-  contentState: ContentState
-  entityKey: string
-}
-
-const TagSpan: React.FunctionComponent<TagSpanProps> = ({
-  contentState,
-  entityKey,
-  children,
-}) => {
-  const data = contentState.getEntity(entityKey).getData()
-
-  const { tagEntries } = useTagEntriesContext()
-
-  const tag = tagEntries.find((tag) => tag.id === data.id)
-
+const TagSpan: React.FunctionComponent = ({ children }) => {
   return (
     <span className="relative">
-      {tag?.name ?? children}
+      {children}
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-yellow-1 opacity-25" />
     </span>
   )
