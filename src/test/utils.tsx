dd3f8c671f6ec49f2c0376661bb495a20d9774f3
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-06-06 13:12:42 -0300

    rename testUtils.tsx to test/utils.tsx

--- src/test/utils.tsx
+++ src/test/utils.tsx
@@ -2,14 +2,16 @@ import { MockedProvider, MockedResponse } from '@apollo/react-testing'
 import { setupI18n } from '@lingui/core'
 import { I18nProvider } from '@lingui/react'
 import { render as rtlRender } from '@testing-library/react'
+import { MemoryHistory, createMemoryHistory } from 'history'
 import { en as enPlural } from 'make-plural/plurals'
 import React, { ReactElement } from 'react'
-import { MemoryRouter } from 'react-router'
+import { Router } from 'react-router'
 
-import enCatalog from './locales/en/messages'
+import enCatalog from '../locales/en/messages'
 
 interface RenderOptions {
   mocks?: MockedResponse[]
+  history?: MemoryHistory
 }
 
 export function render(ui: ReactElement, options?: RenderOptions) {
@@ -20,15 +22,17 @@ export function render(ui: ReactElement, options?: RenderOptions) {
 
   i18n.activate('en')
 
+  const history = options?.history ?? createMemoryHistory()
+
   const utils = rtlRender(
-    <MemoryRouter>
+    <Router history={history}>
       <I18nProvider i18n={i18n}>
         <MockedProvider mocks={options?.mocks} addTypename={false}>
           {ui}
         </MockedProvider>
       </I18nProvider>
-    </MemoryRouter>
+    </Router>
   )
 
-  return utils
+  return { ...utils, history }
 }
