5bad3085e6715464c3c08106562cd3996bbfdb3c
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-07-16 09:40:37 -0300

    Fix deploy CI for the last time

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-git describe --exact-match
+version=$(git describe --exact-match)
 
-if [[ ! $? -eq 0 ]];then
+if [[ ! $? -eq 0 ]]; then
   echo "Nothing to publish, exiting.."
   exit 0;
 fi
@@ -11,7 +11,12 @@ if [[ ! -z "$SENTRY_DSN" ]]; then
   echo "REACT_APP_SENTRY_DSN=$SENTRY_DSN" >> .env
 fi
 
-image_name=$REGISTRY_SERVER/cramkle/cramkle:$VERSION 
+if [[ -z "$REGISTRY_SERVER" ]]; then
+  echo "No registry server, exiting.."
+  exit 0;
+fi
+
+image_name=$REGISTRY_SERVER/cramkle/cramkle:$version
 image_latest=$REGISTRY_SERVER/cramkle/cramkle:latest
 
 docker build -t $image_name .
