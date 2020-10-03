a8161768c9211adfeb79b9fd6b52211ba07d4a40
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-10-03 15:58:08 -0300

    Add better error handling on note save

--- src/components/pages/NotePage.tsx
+++ src/components/pages/NotePage.tsx
@@ -1,5 +1,5 @@
 import { useMutation, useQuery } from '@apollo/react-hooks'
-import { Trans } from '@lingui/macro'
+import { Trans, t } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
 import classnames from 'classnames'
 import { ContentState, RawDraftContentState, convertToRaw } from 'draft-js'
@@ -8,12 +8,15 @@ import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { Helmet } from 'react-helmet'
 import { useParams } from 'react-router'
 
+import { useBlock } from '../../hooks/useBlock'
 import useTopBarLoading from '../../hooks/useTopBarLoading'
+import { notificationState } from '../../notification'
 import BackButton from '../BackButton'
 import FieldValueEditor from '../FieldValueEditor'
 import FlashCardRenderer from '../FlashCardRenderer'
 import FlashCardStatus from '../FlashCardStatus'
 import DoneIcon from '../icons/DoneIcon'
+import RetryIcon from '../icons/RetryIcon'
 import Button from '../views/Button'
 import { Checkbox } from '../views/Checkbox'
 import CircularProgress from '../views/CircularProgress'
@@ -136,7 +139,7 @@ const UPDATE_FIELD_VALUE_MUTATION = gql`
   }
 `
 
-const FIELD_VALUE_CHANGE_DEBOUNCE = 500 /* ms */
+const FIELD_VALUE_CHANGE_DEBOUNCE = 1000 /* ms */
 
 interface FieldValueDetailProps {
   noteId: string
@@ -147,15 +150,18 @@ const FieldValueDetail: React.FC<FieldValueDetailProps> = ({
   noteId,
   fieldValue,
 }) => {
-  const [updateFieldValueMutation, { loading }] = useMutation<
+  const [updateFieldValueMutation, { loading, error }] = useMutation<
     UpdateFieldValue,
     UpdateFieldValueVariables
   >(UPDATE_FIELD_VALUE_MUTATION)
 
   const debounceIdRef = useRef<NodeJS.Timeout | null>(null)
 
+  const lastChangedContentStateRef = useRef<[ContentState, string] | null>(null)
+
   const handleChange = useCallback(
     (content: ContentState, field: { id: string }) => {
+      lastChangedContentStateRef.current = [content, field.id]
       if (debounceIdRef.current) {
         clearTimeout(debounceIdRef.current)
       }
@@ -173,22 +179,57 @@ const FieldValueDetail: React.FC<FieldValueDetailProps> = ({
     [noteId, updateFieldValueMutation]
   )
 
+  const retrySave = useCallback(() => {
+    if (!lastChangedContentStateRef.current) {
+      return
+    }
+
+    const [content, fieldId] = lastChangedContentStateRef.current
+
+    updateFieldValueMutation({
+      variables: {
+        noteId,
+        fieldId,
+        data: convertToRaw(content),
+      },
+    })
+  }, [noteId, updateFieldValueMutation])
+
   const [saved, setSaved] = useState(false)
   const prevLoadingRef = useRef(loading)
 
+  const { i18n } = useLingui()
+
+  useBlock(
+    loading || !!error,
+    i18n._(t`Your note is not saved, are you sure you want to exit the page?`)
+  )
+
   useEffect(() => {
     if (prevLoadingRef.current === loading || loading) {
+      if (loading) {
+        setSaved(false)
+      }
+      return
+    }
+
+    if (error) {
+      notificationState.addNotification({
+        message: i18n._(t`An error occurred when saving your note`),
+        actionText: i18n._(t`Retry`),
+        onAction: retrySave,
+      })
       return
     }
 
     setSaved(true)
-  }, [loading])
+  }, [loading, error, i18n, retrySave])
 
   useEffect(() => {
     prevLoadingRef.current = loading
   }, [loading])
 
-  useEffect(() => {
+  useEffect(function hideSavedMessageEffect() {
     if (!saved) {
       return
     }
@@ -210,19 +251,30 @@ const FieldValueDetail: React.FC<FieldValueDetailProps> = ({
     <>
       <Body1 className="h-8 flex items-center">
         {fieldValue.field.name}{' '}
-        {loading && <CircularProgress className="ml-2" size={16} />}
-        <Caption
-          className={classnames(
-            'inline-flex items-center ml-2 invisible opacity-0',
-            {
-              [styles.fadeIn]: saved,
-              [styles.fadeOut]: prevSavedRef.current && !saved,
-            }
+        <div className="ml-2 flex items-center">
+          {loading && <CircularProgress size={16} />}
+          {error && (
+            <button
+              className="flex items-center text-red-1 text-sm rounded hover:bg-hover-overlay border border-red-1 px-2 py-1"
+              onClick={retrySave}
+            >
+              <RetryIcon className="mr-2 h-4 w-4" />
+              <Trans>Try again</Trans>
+            </button>
           )}
-        >
-          <DoneIcon className="text-green-1 mr-2 text-base" />
-          <Trans>Changes saved successfully</Trans>
-        </Caption>
+          <Caption
+            className={classnames(
+              'inline-flex items-center invisible opacity-0',
+              {
+                [styles.fadeIn]: saved,
+                [styles.fadeOut]: prevSavedRef.current && !saved,
+              }
+            )}
+          >
+            <DoneIcon className="text-green-1 mr-2 text-base" />
+            <Trans>Changes saved successfully</Trans>
+          </Caption>
+        </div>
       </Body1>
       <FieldValueEditor
         className="mb-4 mt-1"
