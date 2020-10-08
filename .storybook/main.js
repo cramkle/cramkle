bac5ed8a743a40d8c481d158b9b88fc1399748ab
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-10-08 09:23:30 -0300

    Add global css to storybook configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,33 @@
+const path = require('path')
+
+const appPath = path.resolve(__dirname, '..')
+
+const buildNodePath = (basePath) => {
+  const nodePath = []
+  const splitted = [''].concat(basePath.split(path.sep))
+
+  for (let i = splitted.length - 1; i > 0; i--) {
+    nodePath.push(path.join(splitted.join(path.sep), 'node_modules'))
+    splitted.pop()
+  }
+
+  return nodePath
+}
+
+const nodePaths = buildNodePath(appPath)
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    {
+      name: '@storybook/preset-scss',
+      options: {
+        sassLoaderOptions: {
+          includePaths: nodePaths,
+        },
+      },
+    },
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+import 'fontsource-libre-franklin/latin.css'
+
+import '../src/_tailwind.global.css'
+import '../src/app.global.scss'
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
