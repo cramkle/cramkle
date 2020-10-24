5dbbdcb67efd194a3b0ed0c873e72666ac752b6a
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-10-24 19:13:47 -0300

    Fix meta descriptions for public pages

--- src/components/pages/AboutPage.tsx
+++ src/components/pages/AboutPage.tsx
@@ -1,7 +1,23 @@
+import { t } from '@lingui/macro'
+import { useLingui } from '@lingui/react'
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 const AboutPage: React.FunctionComponent = () => {
-  return <div>This page is still under development</div>
+  const { i18n } = useLingui()
+
+  return (
+    <div>
+      <Helmet>
+        <title>{i18n._(t`About`)}</title>
+        <meta
+          name="description"
+          content={i18n._(t`About our mission and values, read more`)}
+        />
+      </Helmet>
+      This page is still under development
+    </div>
+  )
 }
 
 export default AboutPage
