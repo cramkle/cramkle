cc1e7b729dc52a1bdc4b1133cdd8066a07d7658f
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-11-20 20:45:55 -0300

    Add percentage diff and fix route matching algo

--- dangerfile.ts
+++ dangerfile.ts
@@ -8,22 +8,27 @@ import filesize from 'filesize'
 import { sync as gzipSize } from 'gzip-size'
 import unzipper from 'unzipper'
 
+const BUILD_DIR = path.join(__dirname, '.dist')
+const BASE_BRANCH_BUILD_DIR = path.join(__dirname, 'build-dist')
+
 const routesManifest = JSON.parse(
-  fs
-    .readFileSync(path.join(__dirname, '.dist', 'routes-manifest.json'))
-    .toString()
+  fs.readFileSync(path.join(BUILD_DIR, 'routes-manifest.json')).toString()
 ) as RoutesManifest
 
-const downloadBuildArtifacts = async () => {
+const downloadBuildArtifacts = async (ref: string) => {
   const {
     data: { workflow_runs: workflowRuns },
   } = await danger.github.api.actions.listWorkflowRunsForRepo({
     owner: 'cramkle',
     repo: 'cramkle',
-    branch: 'main',
+    branch: ref,
   })
 
   const [latestWorkflowRun] = workflowRuns
+
+  if (!latestWorkflowRun) {
+    return null
+  }
 
   const {
     data: { artifacts },
@@ -34,6 +39,10 @@ const downloadBuildArtifacts = async () => {
   })
 
   const buildArtifact = artifacts.find(({ name }) => name === 'build-artifact')
+
+  if (!buildArtifact) {
+    return null
+  }
 
   const { data } = await danger.github.api.actions.downloadArtifact({
     repo: 'cramkle',
@@ -51,15 +60,13 @@ const downloadBuildArtifacts = async () => {
     fs
       .createReadStream(path.join(__dirname, 'build-artifact.zip'))
       .pipe(
-        unzipper
-          .Extract({ path: path.join(__dirname, 'build-dist') })
-          .on('finish', resolve)
+        unzipper.Extract({ path: BASE_BRANCH_BUILD_DIR }).on('finish', resolve)
       )
   )
 
   const mainBranchRoutesManifest = JSON.parse(
     fs
-      .readFileSync(path.join(__dirname, 'build-dist', 'routes-manifest.json'))
+      .readFileSync(path.join(BASE_BRANCH_BUILD_DIR, 'routes-manifest.json'))
       .toString()
   )
 
@@ -79,17 +86,17 @@ function getDifferenceLabel(currentSize: number, previousSize: number) {
   const difference = currentSize - previousSize
   const fileSize = !Number.isNaN(difference) ? filesize(difference) : 0
   if (difference >= FIFTY_KILOBYTES) {
-    return '🔺🔺' + fileSize
+    return '🔺🔺 ' + fileSize
   } else if (difference < FIFTY_KILOBYTES && difference > 0) {
-    return '🔺' + fileSize
+    return '🔺 ' + fileSize
   } else if (difference < 0) {
-    return '🔻' + fileSize
+    return '🔻 ' + fileSize
   }
   return 'No file size difference'
 }
 
 const getFileSizeDifferences = (
-  mainBranchRoutes: RoutesManifest['routes'],
+  baseBranchRoutes: RoutesManifest['routes'],
   currentRoutes: RoutesManifest['routes']
 ) => {
   const sizeDifferences: Array<{
@@ -99,27 +106,38 @@ const getFileSizeDifferences = (
     sizeDifference: number
   }> = []
 
-  mainBranchRoutes.forEach((route, index) => {
-    const mainSize = getSizeForAssets(
-      path.join(__dirname, 'build-dist'),
-      route.assets
+  const usedRoutesSet = new Set<
+    RoutesManifest['routes'] extends Array<infer T> ? T : never
+  >()
+
+  currentRoutes.forEach((route) => {
+    const baseRoute = baseBranchRoutes.find(
+      (baseBranchRoute) =>
+        baseBranchRoute.path === route.path &&
+        !usedRoutesSet.has(baseBranchRoute)
     )
-    const currentSize = getSizeForAssets(
-      path.join(__dirname, '.dist'),
-      currentRoutes[index].assets
-    )
+
+    if (baseRoute) {
+      usedRoutesSet.add(baseRoute)
+    }
+
+    const baseSize = baseRoute
+      ? getSizeForAssets(BASE_BRANCH_BUILD_DIR, baseRoute.assets)
+      : 0
+
+    const currentSize = getSizeForAssets(BUILD_DIR, route.assets)
 
     sizeDifferences.push({
       path: route.path,
-      prevSize: mainSize,
+      prevSize: baseSize,
       size: currentSize,
-      sizeDifference: mainSize - currentSize,
+      sizeDifference: baseSize - currentSize,
     })
 
     if (route.children) {
       const childrenSizeDifferences = getFileSizeDifferences(
-        route.children,
-        currentRoutes[index].children
+        baseRoute?.children ?? [],
+        route.children
       )
 
       childrenSizeDifferences.forEach((childSizeDiff) => {
@@ -131,11 +149,56 @@ const getFileSizeDifferences = (
     }
   })
 
+  baseBranchRoutes.forEach((baseRoute) => {
+    if (usedRoutesSet.has(baseRoute)) {
+      return
+    }
+
+    const assetSize = getSizeForAssets(BASE_BRANCH_BUILD_DIR, baseRoute.assets)
+
+    sizeDifferences.push({
+      path: baseRoute.path,
+      size: 0,
+      prevSize: assetSize,
+      sizeDifference: -assetSize,
+    })
+
+    if (baseRoute.children) {
+      const childrenSizeDifferences = getFileSizeDifferences(
+        baseRoute.children,
+        []
+      )
+
+      childrenSizeDifferences.forEach((childSizeDiff) => {
+        sizeDifferences.push({
+          ...childSizeDiff,
+          path: baseRoute.path + childSizeDiff.path,
+        })
+      })
+    }
+  })
+
   return sizeDifferences
 }
 
-function git(args: string) {
-  return new Promise((res) => {
+const getPercentageDifference = ({
+  size,
+  prevSize,
+}: {
+  size: number
+  prevSize: number
+}) => {
+  if (!prevSize) {
+    return '100%'
+  }
+
+  const diff = (size * 100) / prevSize - 100
+
+  return diff.toFixed(0) + '%'
+}
+
+const git = (args: string) => {
+  return new Promise<string>((res) => {
     exec('git ' + args, (err, stdout) => {
       if (err) {
         throw err
@@ -146,48 +209,54 @@ function git(args: string) {
   })
 }
 
-downloadBuildArtifacts()
-  .catch((error) => {
-    console.error(error)
-    warn('Failed to fetch build artifact for main branch')
+const main = async () => {
+  const upstreamRef = danger.github.pr.base.ref
+
+  await git(`remote add upstream https://github.com/cramkle/cramkle.git`)
+  await git('fetch upstream')
+  const baseCommit = await git(`merge-base HEAD upstream/${upstreamRef}`)
+
+  let baseBranchRoutesManifest
+
+  try {
+    baseBranchRoutesManifest = await downloadBuildArtifacts(upstreamRef)
+  } catch (err) {
+    console.error(err)
+    warn('Failed to fetch build artifact for base commit')
+    return
+  }
+
+  const fileSizeDifferences = getFileSizeDifferences(
+    baseBranchRoutesManifest?.routes ?? [],
+    routesManifest.routes
+  )
+
+  fileSizeDifferences.sort(
+    (a, b) => Math.abs(b.sizeDifference) - Math.abs(a.sizeDifference)
+  )
+
+  const tableBody = fileSizeDifferences.map((fileSizeDiff) => {
+    return `| \`${JSON.stringify(fileSizeDiff.path)}\` | ${getDifferenceLabel(
+      fileSizeDiff.size,
+      fileSizeDiff.prevSize
+    )} | ${getPercentageDifference(fileSizeDiff)} |`
   })
-  .then((mainBranchRoutesManifest) => {
-    if (!mainBranchRoutesManifest) {
-      return
-    }
 
-    const upstreamRef = danger.github.pr.base.ref
+  markdown(`
+## Route file size changes
 
-    return git(`merge-base HEAD upstream/${upstreamRef}`).then((baseCommit) => {
-      const fileSizeDifferences = getFileSizeDifferences(
-        mainBranchRoutesManifest.routes,
-        routesManifest.routes
-      )
-
-      fileSizeDifferences.sort(
-        (a, b) => Math.abs(a.sizeDifference) - Math.abs(b.sizeDifference)
-      )
-
-      const tableBody = fileSizeDifferences.map((fileSizeDiff) => {
-        return `| \`${JSON.stringify(
-          fileSizeDiff.path
-        )}\` | ${getDifferenceLabel(
-          fileSizeDiff.size,
-          fileSizeDiff.prevSize
-        )} |`
-      })
-
-      markdown(`
 <details>
 <summary>Details of assets sizes by route</summary>
 
 <p>Comparing: ${baseCommit}...${danger.github.pr.head.sha}</p>
 
-| Route path | Asset size difference |
-| --- | --- |
+
+| Route path | Asset size difference (gzip) | Percentage difference |
+| --- | --- | --- |
 ${tableBody.join('\n')}
 
 </details>
 `)
-    })
-  })
+}
+
+main()
