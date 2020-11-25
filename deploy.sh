b86fd41c85673c755fa19a48732c5bf4a201fd4a
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-11-25 11:05:49 -0300

    Skip latest tag if version is prerelease

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,16 @@ image_name=$REGISTRY_SERVER/cramkle/cramkle:$version
 image_latest=$REGISTRY_SERVER/cramkle/cramkle:latest
 
 docker build -t $image_name .
-docker tag $image_name $image_latest
+
+echo "Pushing version $version"
+
 docker push $image_name
-docker push $image_latest
+
+# If the version isn't a prerelease, we'll push it
+# using the latest tag as well
+if [[ ! $version =~ - ]]; then
+  echo "Pushing image as latest version"
+
+  docker tag $image_name $image_latest
+  docker push $image_latest
+fi
