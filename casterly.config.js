e8f40452460e0ca88de16c1112410d26431272cd
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-12-08 18:11:11 -0300

    Upgrade dependencies and add workbox plugin

--- casterly.config.js
+++ casterly.config.js
@@ -1,4 +1,37 @@
+const path = require('path')
+
+const webpack = require('webpack')
+const { InjectManifest } = require('workbox-webpack-plugin')
+
+const JS_CSS_REGEX = /\.(?:js|css)$/
+const SOURCE_MAP_REGEX = /\.(?:map)$/
+const IMAGE_REGEX = /\.(?:png|gif|jpg|jpeg|webp|svg)$/
+
+const createWorkboxPlugin = (dev) => {
+  return new InjectManifest({
+    swSrc: path.join(__dirname, 'src/serviceWorker.ts'),
+    swDest: 'public/service-worker.js',
+    exclude: dev
+      ? [JS_CSS_REGEX, IMAGE_REGEX, SOURCE_MAP_REGEX, new RegExp('hot-update')]
+      : [],
+    webpackCompilationPlugins: [
+      new webpack.DefinePlugin(
+        JSON.stringify({
+          'typeof window': JSON.stringify('undefined'),
+        })
+      ),
+    ],
+  })
+}
+
 module.exports = {
+  webpack(config, { dev, isServer }) {
+    if (!isServer) {
+      config.plugins.push(createWorkboxPlugin(dev))
+    }
+
+    return config
+  },
   buildServer: {
     port: 8000,
   },
