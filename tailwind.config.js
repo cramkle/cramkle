887411d14802dc00d7f553f3f44e342cdbd36351
Lucas Cordeiro <ecdb.lucas@gmail.com>
2021-04-20 22:58:22 -0300

    Update tailwind colors and update some packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -86,11 +86,11 @@ module.exports = {
     extend: {
       opacity: {
         '08': '.08',
-        12: '.12',
-        background: 'var(--opacity-background)',
-        disabled: 'var(--opacity-disabled)',
-        divider: 'var(--opacity-divider)',
-        secondary: 'var(--opacity-secondary)',
+        '12': '.12',
+        'background': 'var(--opacity-background)',
+        'disabled': 'var(--opacity-disabled)',
+        'divider': 'var(--opacity-divider)',
+        'secondary': 'var(--opacity-secondary)',
         'text-primary': 'var(--opacity-text-primary)',
         'text-secondary': 'var(--opacity-text-secondary)',
         'text-icon': 'var(--opacity-text-icon)',
@@ -100,17 +100,18 @@ module.exports = {
       zIndex: {
         '-10': '-10',
         '-1': '-1',
-        1: '1',
+        '1': '1',
       },
       maxWidth: {
         xxs: '12rem',
       },
       boxShadow: {
-        sm: '0 1px 2px rgba(0, 0, 0, 0.05)',
-        DEFAULT: '0 3px 5px rgba(0, 0, 0, .15)',
-        md: '0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 3px 5px 0 rgba(0, 0, 0, 0.15)',
-        lg: '0 12px 28px 0 rgba(0, 0, 0, 0.1), 0 4px 8px 0 rgba(0, 0, 0, 0.15)',
-        xl:
+        'sm': '0 1px 2px rgba(0, 0, 0, 0.05)',
+        'DEFAULT': '0 3px 5px rgba(0, 0, 0, .15)',
+        'md': '0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 3px 5px 0 rgba(0, 0, 0, 0.15)',
+        'lg':
+          '0 12px 28px 0 rgba(0, 0, 0, 0.1), 0 4px 8px 0 rgba(0, 0, 0, 0.15)',
+        'xl':
           '0 32px 48px 0 rgba(0, 0, 0, 0.1), 0 12px 28px 0 rgba(0, 0, 0, 0.15)',
         '2xl':
           '0 25px 50px 0 rgba(0, 0, 0, 0.25), 0 32px 48px 0 rgba(0, 0, 0, .15)',
@@ -121,6 +122,8 @@ module.exports = {
     extend: {
       display: ['group-hover', 'group-focus'],
       opacity: ['group-focus'],
+      textColor: ['group-focus'],
+      backgroundColor: ['group-focus'],
     },
   },
   purge: {
