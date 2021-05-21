7170cb394ae83a585725d3f6b2de8401d1b953cf
Lucas Cordeiro <ecdb.lucas@gmail.com>
2021-05-21 09:47:17 -0300

    Add canary deploying on every main commit (#564)
    * Add canary deploying on every main commit
    
    * Fix variable name
    
    * Move deploy CI to separate workflow

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,12 @@
 
 unparsed_version=$(git describe --exact-match)
 
-if [[ ! $? -eq 0 ]]; then
-  echo "Nothing to publish, exiting.."
-  exit 0;
-fi
-
-version=${unparsed_version//v}
-
-[[ ! $version =~ - ]]
 is_stable=$?
 
 if [[ ! -z "$SENTRY_DSN" ]] && (( is_stable == 0 )); then
   echo "CASTERLY_PUBLIC_SENTRY_DSN=$SENTRY_DSN" >> .env
 else
-  echo "No SENTRY_DSN, skipping Sentry config.."
+  echo "No SENTRY_DSN or not stable build, skipping Sentry config.."
 fi
 
 if [[ -z "$REGISTRY_SERVER" ]]; then
@@ -33,20 +25,32 @@ while [[ "$#" -gt 0 ]]; do
   shift
 done
 
-image_name=$REGISTRY_SERVER/cramkle:$version
-image_latest=$REGISTRY_SERVER/cramkle:latest
+commit_hash=$(git rev-parse --short HEAD)
+
+image_name=$REGISTRY_SERVER/cramkle:$commit_hash
 
 docker build -t $image_name -f $dockerfile .
-
-echo "Pushing version $version"
-
-docker push $image_name
 
 # If the version isn't a prerelease, we'll push it
 # using the latest tag as well
 if (( is_stable == 0 )); then
+  version=${unparsed_version//v}
+
   echo "Pushing image as latest version"
 
+  image_versioned=$REGISTRY_SERVER/cramkle:$version
+  image_latest=$REGISTRY_SERVER/cramkle:latest
+
   docker tag $image_name $image_latest
+
+  docker push $image_versioned
   docker push $image_latest
+else
+  echo "Pushing canary image"
+
+  image_canary=$REGISTRY_SERVER/cramkle:canary
+
+  docker tag $image_name $image_canary
+
+  docker push $image_canary
 fi
