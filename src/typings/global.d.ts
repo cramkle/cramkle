a45890aea4bbd9387b4d90292816c746e833a2cf
Lucas Cordeiro <ecdb.lucas@gmail.com>
2021-07-11 12:54:53 -0300

    Remove globals file

--- src/typings/global.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="graphql" />
-
-import type { NormalizedCacheObject } from 'apollo-cache-inmemory'
-
-declare global {
-  interface Window {
-    __APOLLO_STATE__: NormalizedCacheObject
-  }
-}
